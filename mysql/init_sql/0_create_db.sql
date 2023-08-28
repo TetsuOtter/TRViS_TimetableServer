542adfb779182aa6dcc702f6c3fa1bc2f2004839
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2023-08-29 01:24:25 +0900

    テーブル構造を作成
    残りを全て完成させた

--- mysql/init_sql/0_create_db.sql
+++ mysql/init_sql/0_create_db.sql
@@ -87,6 +87,92 @@ CREATE TABLE
 );
 
 CREATE TABLE
+  colors
+(
+  colors_id
+    BINARY(16)
+    NOT NULL
+    COMMENT 'UUID v4'
+  ,
+
+  work_groups_id
+    BINARY(16)
+    NOT NULL
+    COMMENT 'UUID v4'
+  ,
+
+  created_at
+    DATETIME
+    NOT NULL
+    DEFAULT CURRENT_TIMESTAMP
+  ,
+
+  updated_at
+    DATETIME
+    NOT NULL
+    DEFAULT CURRENT_TIMESTAMP
+    ON UPDATE CURRENT_TIMESTAMP
+  ,
+
+  owner
+    VARCHAR(255)
+    NOT NULL
+  ,
+
+  description
+    VARCHAR(255)
+    NOT NULL
+  ,
+
+  name
+    VARCHAR(255)
+    UNIQUE
+    NOT NULL
+  ,
+
+  red_8bit
+    TINYINT UNSIGNED
+    NOT NULL
+  ,
+
+  green_8bit
+    TINYINT UNSIGNED
+    NOT NULL
+  ,
+
+  blue_8bit
+    TINYINT UNSIGNED
+    NOT NULL
+  ,
+
+  red_real
+    DECIMAL(16, 15)
+    NOT NULL
+  ,
+
+  green_real
+    DECIMAL(16, 15)
+    NOT NULL
+  ,
+
+  blue_real
+    DECIMAL(16, 15)
+    NOT NULL
+  ,
+
+  PRIMARY KEY (
+    colors_id
+  ),
+
+  FOREIGN KEY (
+    work_groups_id
+  ) REFERENCES
+  work_groups (
+    work_groups_id
+  )
+);
+
+CREATE TABLE
   works
 (
   works_id
@@ -284,5 +370,304 @@ CREATE TABLE
   ) REFERENCES
   works (
     works_id
+  )
+);
+
+CREATE TABLE
+  stations
+(
+  stations_id
+    BINARY(16)
+    NOT NULL
+    COMMENT 'UUID v4'
+  ,
+
+  work_groups_id
+    BINARY(16)
+    NOT NULL
+    COMMENT 'UUID v4'
+  ,
+
+  description
+    VARCHAR(255)
+    NOT NULL
+  ,
+
+  owner
+    VARCHAR(255)
+    NOT NULL
+  ,
+
+  created_at
+    DATETIME
+    NOT NULL
+    DEFAULT CURRENT_TIMESTAMP
+  ,
+
+  updated_at
+    DATETIME
+    NOT NULL
+    DEFAULT CURRENT_TIMESTAMP
+    ON UPDATE CURRENT_TIMESTAMP
+  ,
+
+  name
+    VARCHAR(255)
+    NOT NULL
+  ,
+
+  location_km
+    DOUBLE PRECISION
+    NOT NULL
+  ,
+
+  location_lonlat
+    POINT
+  ,
+
+  on_station_detect_radius_m
+    DOUBLE PRECISION
+    NOT NULL
+  ,
+
+  PRIMARY KEY (
+    stations_id
+  ),
+
+  FOREIGN KEY (
+    work_groups_id
+  ) REFERENCES
+  work_groups (
+    work_groups_id
+  )
+);
+
+CREATE TABLE
+  station_tracks
+(
+  station_tracks_id
+    BINARY(16)
+    NOT NULL
+    COMMENT 'UUID v4'
+  ,
+
+  stations_id
+    BINARY(16)
+    NOT NULL
+    COMMENT 'UUID v4'
+  ,
+
+  description
+    VARCHAR(255)
+    NOT NULL
+  ,
+
+  owner
+    VARCHAR(255)
+    NOT NULL
+  ,
+
+  created_at
+    DATETIME
+    NOT NULL
+    DEFAULT CURRENT_TIMESTAMP
+  ,
+
+  updated_at
+    DATETIME
+    NOT NULL
+    DEFAULT CURRENT_TIMESTAMP
+    ON UPDATE CURRENT_TIMESTAMP
+  ,
+
+  name
+    VARCHAR(255)
+    NOT NULL
+  ,
+
+  run_in_limit
+    SMALLINT
+  ,
+
+  run_out_limit
+    SMALLINT
+  ,
+
+  FOREIGN KEY (
+    stations_id
+  ) REFERENCES
+  stations (
+    stations_id
+  )
+);
+
+CREATE TABLE
+  timetable_rows
+(
+  timetable_rows_id
+    BINARY(16)
+    NOT NULL
+    COMMENT 'UUID v4'
+  ,
+
+  trains_id
+    BINARY(16)
+    NOT NULL
+    COMMENT 'UUID v4'
+  ,
+
+  stations_id
+    BINARY(16)
+    NOT NULL
+    COMMENT 'UUID v4'
+  ,
+
+  station_tracks_id
+    BINARY(16)
+    COMMENT 'UUID v4'
+  ,
+
+  colors_id
+    BINARY(16)
+    COMMENT 'UUID v4'
+  ,
+
+  description
+    VARCHAR(255)
+    NOT NULL
+  ,
+
+  owner
+    VARCHAR(255)
+    NOT NULL
+  ,
+
+  created_at
+    DATETIME
+    NOT NULL
+    DEFAULT CURRENT_TIMESTAMP
+  ,
+
+  updated_at
+    DATETIME
+    NOT NULL
+    DEFAULT CURRENT_TIMESTAMP
+    ON UPDATE CURRENT_TIMESTAMP
+  ,
+
+  drive_time_mm
+    TINYINT(3) UNSIGNED
+  ,
+
+  drive_time_ss
+    TINYINT(2) UNSIGNED
+  ,
+
+  is_operation_only_stop
+    BOOLEAN
+    NOT NULL
+    DEFAULT FALSE
+  ,
+
+  is_pass
+    BOOLEAN
+    NOT NULL
+    DEFAULT FALSE
+  ,
+
+  has_bracket
+    BOOLEAN
+    NOT NULL
+    DEFAULT FALSE
+  ,
+
+  is_last_stop
+    BOOLEAN
+    NOT NULL
+    DEFAULT FALSE
+  ,
+
+  arrive_hh
+    TINYINT(2) UNSIGNED
+  ,
+
+  arrive_mm
+    TINYINT(2) UNSIGNED
+  ,
+
+  arrive_ss
+    TINYINT(2) UNSIGNED
+  ,
+
+  departure_hh
+    TINYINT(2) UNSIGNED
+  ,
+
+  departure_mm
+    TINYINT(2) UNSIGNED
+  ,
+
+  departure_ss
+    TINYINT(2) UNSIGNED
+  ,
+
+  run_in_limit
+    SMALLINT
+  ,
+
+  run_out_limit
+    SMALLINT
+  ,
+
+  remarks
+    VARCHAR(65535)
+  ,
+
+  arrive_str
+    VARCHAR(255)
+  ,
+
+  departure_str
+    VARCHAR(255)
+  ,
+
+  marker_text
+    VARCHAR(16)
+  ,
+
+  work_type
+    TINYINT UNSIGNED
+  ,
+
+  PRIMARY KEY (
+    timetable_rows_id
+  ),
+
+  FOREIGN KEY (
+    trains_id
+  ) REFERENCES
+  trains (
+    trains_id
+  ),
+
+  FOREIGN KEY (
+    stations_id
+  ) REFERENCES
+  stations (
+    stations_id
+  ),
+
+  FOREIGN KEY (
+    station_tracks_id
+  ) REFERENCES
+  station_tracks (
+    station_tracks_id
+  ),
+
+  FOREIGN KEY (
+    colors_id
+  ) REFERENCES
+  colors (
+    colors_id
   )
 );
