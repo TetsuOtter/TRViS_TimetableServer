8cc8d7e2b960a3a19b3be59cbcf38d6232d71b0e
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2023-12-29 22:11:50 +0900

    php-slim4にした
    Laravelは学習コストが高そう…

--- api_defs/gen_php.sh
+++ api_defs/gen_php.sh
@@ -4,6 +4,6 @@ cd $(dirname $0)/../
 
 openapi-generator generate \
     -i api_defs/openapi.bundle.yml \
-    -c api_defs/php-laravel.config.yaml \
-    -g php-laravel \
+    -c api_defs/php-slim4.config.yaml \
+    -g php-slim4 \
     -o backend \
