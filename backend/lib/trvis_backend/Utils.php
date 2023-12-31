5fb8594e24728784584a191a64e453d8b6b495f7
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2023-12-31 18:15:36 +0900

    エラー出力系ヘルパー関数を追加

--- backend/lib/trvis_backend/Utils.php
+++ backend/lib/trvis_backend/Utils.php
@@ -16,4 +16,29 @@ final class Utils
 		$response->getBody()->write(json_encode($data));
 		return $response;
 	}
+
+	public static function withError(
+		\Psr\Http\Message\ResponseInterface $oldResponse,
+		int $statusCode,
+		string $message,
+		int $errorCode = null,
+	): \Psr\Http\Message\ResponseInterface {
+		if ($errorCode === null) {
+			$errorCode = $statusCode;
+		}
+
+		return self::withJson(
+			$oldResponse, [
+				'code' => $errorCode,
+				'message' => $message,
+			],
+			$statusCode,
+		);
+	}
+
+	public static function withUuidError(
+		\Psr\Http\Message\ResponseInterface $oldResponse,
+	): \Psr\Http\Message\ResponseInterface {
+		return self::withError($oldResponse, 400, 'Bad Request (Invalid UUID format)');
+	}
 }
