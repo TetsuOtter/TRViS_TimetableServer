c51a3219f9f13a54fdeea8d22c0af711eb204912
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2024-01-07 03:05:02 +0900

    InviteKeyPrivilegeTypeはSerializableなので、そのままInviteKeyに記録するようにした

--- backend/src/trvis_backend/repo/InviteKeys.php
+++ backend/src/trvis_backend/repo/InviteKeys.php
@@ -46,7 +46,7 @@ final class InviteKeys
 			'expires_at' => Utils::dbDateStrToDateTime($data['expires_at']),
 			'disabled_at' => Utils::dbDateStrToDateTime($data['disabled_at']),
 			'use_limit' => $data['use_limit'],
-			'privilege_type' => InviteKeyPrivilegeType::fromInt($data['privilege_type'])->name,
+			'privilege_type' => InviteKeyPrivilegeType::fromInt($data['privilege_type']),
 		]);
 		return $inviteKey;
 	}
