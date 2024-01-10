4f56f2341dc2e9370ad8cebb09d5549c68e771f1
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2024-01-10 22:40:46 +0900

    Api側も実装を共通化し、Stationに適用

---
+++ backend/src/trvis_backend/api/MyApiHandler.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace dev_t0r\trvis_backend\api;
+
+use dev_t0r\trvis_backend\auth\MyAuthMiddleware;
+use dev_t0r\trvis_backend\Constants;
+use dev_t0r\trvis_backend\RetValueOrError;
+use dev_t0r\trvis_backend\service\MyServiceBase;
+use dev_t0r\trvis_backend\Utils;
+use dev_t0r\trvis_backend\validator\PagingQueryValidator;
+use dev_t0r\trvis_backend\validator\RequestValidator;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerInterface;
+use Ramsey\Uuid\Uuid;
+
+class MyApiHandler
+{
+	public function __construct(
+		private readonly MyServiceBase $service,
+		private readonly LoggerInterface $logger,
+		private readonly string $modelClassName,
+		private readonly RequestValidator $bodyValidator,
+	) {
+	}
+
+	public function create(
+		ServerRequestInterface $request,
+		ResponseInterface $response,
+		string $parentId,
+	): ResponseInterface {
+		$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
+
+		if (!Uuid::isValid($parentId))
+		{
+			$this->logger->warning("Invalid UUID format ({parentId})", ['parentId' => $parentId]);
+			return Utils::withUuidError($response);
+		}
+
+		$body = $request->getParsedBody();
+		$validateResult = $this->bodyValidator->validate(
+			d: $body,
+			checkRequired: true,
+			allowNestedArray: true,
+		);
+		if ($validateResult->isError) {
+			$this->logger->error(
+				"invalid input: {message}",
+				[
+					"message" => $validateResult->errorMsg,
+				],
+			);
+			return $validateResult->getResponseWithJson($response);
+		}
+
+		$isSingleItemRequest = !is_array($body) || !array_is_list($body);
+		try
+		{
+			$stationsList = array_map(function ($req) {
+				$station = new $this->modelClassName();
+				$station->setData($req);
+				return $station;
+			}, ($isSingleItemRequest) ? [$body] : $body);
+		}
+		catch (\Throwable $e)
+		{
+			$this->logger->error(
+				'Failed to create {modelClassName} object: {exception}',
+				[
+					'modelClassName' => $this->modelClassName,
+					'exception' => $e,
+				],
+			);
+			return Utils::withError(
+				$response,
+				Constants::HTTP_BAD_REQUEST,
+				"Failed to create {$this->modelClassName} object - " . $e->getMessage(),
+			);
+		}
+
+		$createResult = $this->service->create(
+			parentId: Uuid::fromString($parentId),
+			senderUserId: $userId,
+			dataList: $stationsList,
+		);
+		if (!$createResult->isError && $isSingleItemRequest) {
+			$createResult = RetValueOrError::withValue($createResult->value[0]);
+		}
+		return $createResult->getResponseWithJson($response);
+	}
+
+	public function delete(
+		ServerRequestInterface $request,
+		ResponseInterface $response,
+		string $id,
+		): ResponseInterface {
+			$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
+
+			if (!Uuid::isValid($id))
+			{
+				$this->logger->warning("Invalid UUID format ({id})", ['id' => $id]);
+				return Utils::withUuidError($response);
+			}
+
+			return $this->service->delete(
+				senderUserId: $userId,
+				id: Uuid::fromString($id),
+			)->getResponseWithJson($response);
+		}
+
+	public function getOne(
+		ServerRequestInterface $request,
+		ResponseInterface $response,
+		string $id,
+		): ResponseInterface {
+			$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
+
+			if (!Uuid::isValid($id))
+			{
+				$this->logger->warning("Invalid UUID format ({id})", ['id' => $id]);
+				return Utils::withUuidError($response);
+			}
+
+			return $this->service->getOne(
+				senderUserId: $userId,
+				id: Uuid::fromString($id),
+			)->getResponseWithJson($response);
+		}
+
+	public function getPage(
+		ServerRequestInterface $request,
+		ResponseInterface $response,
+		string $parentId,
+		): ResponseInterface {
+			$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
+
+			if (!Uuid::isValid($parentId))
+			{
+				$this->logger->warning("Invalid UUID format ({parentId})", ['parentId' => $parentId]);
+				return Utils::withUuidError($response);
+			}
+
+			$pagingParams = PagingQueryValidator::withRequest($request, $this->logger);
+			if ($pagingParams->isError) {
+				return $pagingParams->reqError->getResponseWithJson($response);
+			}
+
+			return $this->service->getPage(
+				parentId: Uuid::fromString($parentId),
+				senderUserId: $userId,
+				pageFrom1: $pagingParams->pageFrom1,
+				perPage: $pagingParams->perPage,
+				topId: $pagingParams->topId,
+			)->getResponseWithJson($response);
+		}
+
+	public function update(
+		ServerRequestInterface $request,
+		ResponseInterface $response,
+		string $id
+		): ResponseInterface {
+			$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
+
+			if (!Uuid::isValid($id))
+			{
+				$this->logger->warning("Invalid UUID format ({id})", ['id' => $id]);
+				return Utils::withUuidError($response);
+			}
+
+			$body = $request->getParsedBody();
+			if (is_null($body)) {
+				$this->logger->warning('empty request body');
+				return Utils::withError($response, Constants::HTTP_BAD_REQUEST, 'empty request body');
+			}
+
+			$validateBodyResult = $this->bodyValidator->validate(
+				d: $body,
+				checkRequired: false,
+				allowNestedArray: false,
+			);
+			if ($validateBodyResult->isError) {
+				$this->logger->warning(
+					'invalid request body: {message}',
+					[
+						'message' => $validateBodyResult->errorMsg,
+					],
+				);
+				return $validateBodyResult->getResponseWithJson($response);
+			}
+			$data = new $this->modelClassName();
+			$data->setData($body);
+			return $this->service->update(
+				senderUserId: $userId,
+				stationsId: Uuid::fromString($id),
+				data: $data,
+				requestBody: $body,
+			)->getResponseWithJson($response);
+		}
+	}
--- backend/src/trvis_backend/api/StationApi.php
+++ backend/src/trvis_backend/api/StationApi.php
@@ -2,57 +2,54 @@
 
 namespace dev_t0r\trvis_backend\api;
 
-use dev_t0r\trvis_backend\auth\MyAuthMiddleware;
-use dev_t0r\trvis_backend\Constants;
 use dev_t0r\trvis_backend\model\Station;
 use dev_t0r\trvis_backend\model\StationRecordType;
-use dev_t0r\trvis_backend\RetValueOrError;
 use dev_t0r\trvis_backend\service\StationsService;
-use dev_t0r\trvis_backend\Utils;
 use dev_t0r\trvis_backend\validator\EnumValidationRule;
 use dev_t0r\trvis_backend\validator\FloatValidationRule;
 use dev_t0r\trvis_backend\validator\LonLatValidationRule;
-use dev_t0r\trvis_backend\validator\PagingQueryValidator;
 use dev_t0r\trvis_backend\validator\RequestValidator;
 use PDO;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Log\LoggerInterface;
-use Ramsey\Uuid\Uuid;
 
 final class StationApi extends AbstractStationApi
 {
-	private readonly StationsService $stationsService;
-	private readonly RequestValidator $bodyValidator;
+	private readonly MyApiHandler $apiHandler;
 	public function __construct(
-		private PDO $db,
+		PDO $db,
 		private readonly LoggerInterface $logger,
 	) {
-		$this->stationsService = new StationsService($db, $logger);
-		$this->bodyValidator = new RequestValidator(
-			RequestValidator::getNameValidationRule(),
-			RequestValidator::getDescriptionValidationRule(),
-			new FloatValidationRule(
-				key: 'location_km',
-				isNullable: false,
-				isRequired: true,
-			),
-			new LonLatValidationRule(
-				key: 'location_lonlat',
-				isNullable: true,
-				isRequired: false,
-			),
-			new FloatValidationRule(
-				key: 'on_station_detect_radius_m',
-				isNullable: false,
-				isRequired: true,
-				minValue: 0.0,
-			),
-			new EnumValidationRule(
-				key: 'record_type',
-				isNullable: false,
-				isRequired: true,
-				className: StationRecordType::class,
+		$this->apiHandler = new MyApiHandler(
+			service: new StationsService($db, $logger),
+			logger: $logger,
+			modelClassName: Station::class,
+			bodyValidator: new RequestValidator(
+				RequestValidator::getNameValidationRule(),
+				RequestValidator::getDescriptionValidationRule(),
+				new FloatValidationRule(
+					key: 'location_km',
+					isNullable: false,
+					isRequired: true,
+				),
+				new LonLatValidationRule(
+					key: 'location_lonlat',
+					isNullable: true,
+					isRequired: false,
+				),
+				new FloatValidationRule(
+					key: 'on_station_detect_radius_m',
+					isNullable: false,
+					isRequired: true,
+					minValue: 0.0,
+				),
+				new EnumValidationRule(
+					key: 'record_type',
+					isNullable: false,
+					isRequired: true,
+					className: StationRecordType::class,
+				),
 			),
 		);
 	}
@@ -62,58 +59,11 @@ final class StationApi extends AbstractStationApi
 		ResponseInterface $response,
 		string $workGroupId
 	): ResponseInterface {
-		$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-		if (!Uuid::isValid($workGroupId))
-		{
-			$this->logger->warning("Invalid UUID format ({workGroupId})", ['workGroupId' => $workGroupId]);
-			return Utils::withUuidError($response);
-		}
-
-		$body = $request->getParsedBody();
-		$validateResult = $this->bodyValidator->validate(
-			d: $body,
-			checkRequired: true,
-			allowNestedArray: true,
+		return $this->apiHandler->create(
+			request: $request,
+			response: $response,
+			parentId: $workGroupId,
 		);
-		if ($validateResult->isError) {
-			$this->logger->error(
-				"invalid input: {message}",
-				[
-					"message" => $validateResult->errorMsg,
-				],
-			);
-			return $validateResult->getResponseWithJson($response);
-		}
-
-		try
-		{
-			$stationsList = array_map(function ($req) {
-				$station = new Station();
-				$station->setData($req);
-				return $station;
-			}, (is_array($body) && array_is_list($body)) ? $body : [$body]);
-		}
-		catch (\Throwable $e)
-		{
-			$this->logger->error(
-				'Failed to create Station object: {exception}',
-				[
-					'exception' => $e,
-				],
-			);
-			return Utils::withError($response, Constants::HTTP_BAD_REQUEST, 'Failed to create Station object - ' . $e->getMessage());
-		}
-
-		$createResult = $this->stationsService->create(
-			parentId: Uuid::fromString($workGroupId),
-			senderUserId: $userId,
-			dataList: $stationsList,
-		);
-		if (!$createResult->isError && (!is_array($body) || !array_is_list($body))) {
-			$createResult = RetValueOrError::withValue($createResult->value[0]);
-		}
-		return $createResult->getResponseWithJson($response);
 	}
 
 	public function deleteStation(
@@ -121,18 +71,11 @@ final class StationApi extends AbstractStationApi
 		ResponseInterface $response,
 		string $stationId
 		): ResponseInterface {
-			$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-			if (!Uuid::isValid($stationId))
-			{
-				$this->logger->warning("Invalid UUID format ({stationId})", ['stationId' => $stationId]);
-				return Utils::withUuidError($response);
-			}
-
-			return $this->stationsService->delete(
-				senderUserId: $userId,
-				id: Uuid::fromString($stationId),
-			)->getResponseWithJson($response);
+			return $this->apiHandler->delete(
+				request: $request,
+				response: $response,
+				id: $stationId,
+			);
 		}
 
 	public function getStation(
@@ -140,18 +83,11 @@ final class StationApi extends AbstractStationApi
 		ResponseInterface $response,
 		string $stationId
 		): ResponseInterface {
-			$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-			if (!Uuid::isValid($stationId))
-			{
-				$this->logger->warning("Invalid UUID format ({stationId})", ['stationId' => $stationId]);
-				return Utils::withUuidError($response);
-			}
-
-			return $this->stationsService->getOne(
-				senderUserId: $userId,
-				id: Uuid::fromString($stationId),
-			)->getResponseWithJson($response);
+			return $this->apiHandler->getOne(
+				request: $request,
+				response: $response,
+				id: $stationId,
+			);
 		}
 
 	public function getStationList(
@@ -159,26 +95,11 @@ final class StationApi extends AbstractStationApi
 		ResponseInterface $response,
 		string $workGroupId
 		): ResponseInterface {
-			$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-			if (!Uuid::isValid($workGroupId))
-			{
-				$this->logger->warning("Invalid UUID format ({workGroupId})", ['workGroupId' => $workGroupId]);
-				return Utils::withUuidError($response);
-			}
-
-			$pagingParams = PagingQueryValidator::withRequest($request, $this->logger);
-			if ($pagingParams->isError) {
-				return $pagingParams->reqError->getResponseWithJson($response);
-			}
-
-			return $this->stationsService->getPage(
-				parentId: Uuid::fromString($workGroupId),
-				senderUserId: $userId,
-				pageFrom1: $pagingParams->pageFrom1,
-				perPage: $pagingParams->perPage,
-				topId: $pagingParams->topId,
-			)->getResponseWithJson($response);
+			return $this->apiHandler->getPage(
+				request: $request,
+				response: $response,
+				parentId: $workGroupId,
+			);
 		}
 
 	public function updateStation(
@@ -186,41 +107,10 @@ final class StationApi extends AbstractStationApi
 		ResponseInterface $response,
 		string $stationId
 		): ResponseInterface {
-			$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-			if (!Uuid::isValid($stationId))
-			{
-				$this->logger->warning("Invalid UUID format ({stationId})", ['stationId' => $stationId]);
-				return Utils::withUuidError($response);
-			}
-
-			$body = $request->getParsedBody();
-			if (is_null($body)) {
-				$this->logger->warning('empty request body');
-				return Utils::withError($response, Constants::HTTP_BAD_REQUEST, 'empty request body');
-			}
-
-			$validateBodyResult = $this->bodyValidator->validate(
-				d: $body,
-				checkRequired: false,
-				allowNestedArray: false,
+			return $this->apiHandler->update(
+				request: $request,
+				response: $response,
+				id: $stationId,
 			);
-			if ($validateBodyResult->isError) {
-				$this->logger->warning(
-					'invalid request body: {message}',
-					[
-						'message' => $validateBodyResult->errorMsg,
-					],
-				);
-				return $validateBodyResult->getResponseWithJson($response);
-			}
-			$stationData = new Station();
-			$stationData->setData($body);
-			return $this->stationsService->update(
-				senderUserId: $userId,
-				stationsId: Uuid::fromString($stationId),
-				data: $stationData,
-				requestBody: $body,
-			)->getResponseWithJson($response);
 		}
 	}
