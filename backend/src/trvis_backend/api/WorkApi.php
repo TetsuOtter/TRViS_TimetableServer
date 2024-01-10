456a7ce17d7151750e8c9ecc27a4f53926a05874
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2024-01-10 23:02:25 +0900

    Work周りも実装を共通化

--- backend/src/trvis_backend/api/WorkApi.php
+++ backend/src/trvis_backend/api/WorkApi.php
@@ -2,77 +2,75 @@
 
 namespace dev_t0r\trvis_backend\api;
 
-use dev_t0r\trvis_backend\auth\MyAuthMiddleware;
-use dev_t0r\trvis_backend\Constants;
 use dev_t0r\trvis_backend\model\TrvisContentType;
 use dev_t0r\trvis_backend\model\Work;
-use dev_t0r\trvis_backend\RetValueOrError;
 use dev_t0r\trvis_backend\service\WorksService;
-use dev_t0r\trvis_backend\Utils;
 use dev_t0r\trvis_backend\validator\BoolValidationRule;
 use dev_t0r\trvis_backend\validator\DateTimeValidationRule;
 use dev_t0r\trvis_backend\validator\EnumValidationRule;
-use dev_t0r\trvis_backend\validator\PagingQueryValidator;
 use dev_t0r\trvis_backend\validator\RequestValidator;
 use dev_t0r\trvis_backend\validator\StringValidationRule;
 use PDO;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Log\LoggerInterface;
-use Ramsey\Uuid\Uuid;
 
-class WorkApi extends AbstractWorkApi
+final class WorkApi extends AbstractWorkApi
 {
+
 	const REMARKS_MAX_LENGTH = 255;
 
-	private readonly WorksService $worksService;
-	private readonly RequestValidator $bodyValidator;
+	private readonly MyApiHandler $apiHandler;
 	public function __construct(
-		private readonly PDO $db,
+		PDO $db,
 		private readonly LoggerInterface $logger,
 	) {
-		$this->worksService = new WorksService($db, $logger);
-		$this->bodyValidator = new RequestValidator(
-			RequestValidator::getNameValidationRule(),
-			RequestValidator::getDescriptionValidationRule(),
-			new DateTimeValidationRule(
-				key: 'affect_date',
-				isNullable: true,
-				isRequired: false,
-				isDateOnly: true,
-			),
-			new EnumValidationRule(
-				key: 'affix_content_type',
-				isNullable: true,
-				isRequired: false,
-				className: TrvisContentType::class,
-			),
-			new StringValidationRule(
-				key: 'affix_content',
-				isNullable: true,
-				isRequired: false,
-			),
-			new StringValidationRule(
-				key: 'remarks',
-				isNullable: true,
-				isRequired: false,
-				maxLength: self::REMARKS_MAX_LENGTH,
-			),
-			new BoolValidationRule(
-				key: 'has_e_train_timetable',
-				isNullable: true,
-				isRequired: false,
-			),
-			new EnumValidationRule(
-				key: 'e_train_timetable_content_type',
-				isNullable: true,
-				isRequired: false,
-				className: TrvisContentType::class,
-			),
-			new StringValidationRule(
-				key: 'e_train_timetable_content',
-				isNullable: true,
-				isRequired: false,
+		$this->apiHandler = new MyApiHandler(
+			service: new WorksService($db, $logger),
+			logger: $logger,
+			modelClassName: Work::class,
+			bodyValidator: new RequestValidator(
+				RequestValidator::getNameValidationRule(),
+				RequestValidator::getDescriptionValidationRule(),
+				new DateTimeValidationRule(
+					key: 'affect_date',
+					isNullable: true,
+					isRequired: false,
+					isDateOnly: true,
+				),
+				new EnumValidationRule(
+					key: 'affix_content_type',
+					isNullable: true,
+					isRequired: false,
+					className: TrvisContentType::class,
+				),
+				new StringValidationRule(
+					key: 'affix_content',
+					isNullable: true,
+					isRequired: false,
+				),
+				new StringValidationRule(
+					key: 'remarks',
+					isNullable: true,
+					isRequired: false,
+					maxLength: self::REMARKS_MAX_LENGTH,
+				),
+				new BoolValidationRule(
+					key: 'has_e_train_timetable',
+					isNullable: true,
+					isRequired: false,
+				),
+				new EnumValidationRule(
+					key: 'e_train_timetable_content_type',
+					isNullable: true,
+					isRequired: false,
+					className: TrvisContentType::class,
+				),
+				new StringValidationRule(
+					key: 'e_train_timetable_content',
+					isNullable: true,
+					isRequired: false,
+				),
 			),
 		);
 	}
@@ -82,58 +80,11 @@ class WorkApi extends AbstractWorkApi
 		ResponseInterface $response,
 		string $workGroupId
 	): ResponseInterface {
-		$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-		if (!Uuid::isValid($workGroupId))
-		{
-			$this->logger->warning("Invalid UUID format ({workGroupId})", ['workGroupId' => $workGroupId]);
-			return Utils::withUuidError($response);
-		}
-
-		$body = $request->getParsedBody();
-		$validateResult = $this->bodyValidator->validate(
-			d: $body,
-			checkRequired: true,
-			allowNestedArray: true,
+		return $this->apiHandler->create(
+			request: $request,
+			response: $response,
+			parentId: $workGroupId,
 		);
-		if ($validateResult->isError) {
-			$this->logger->error(
-				"invalid input: {message}",
-				[
-					"message" => $validateResult->errorMsg,
-				],
-			);
-			return $validateResult->getResponseWithJson($response);
-		}
-
-		try
-		{
-			$worksList = array_map(function ($req) {
-				$work = new Work();
-				$work->setData($req);
-				return $work;
-			}, (is_array($body) && array_is_list($body)) ? $body : [$body]);
-		}
-		catch (\Throwable $e)
-		{
-			$this->logger->error(
-				'Failed to create Work object: {exception}',
-				[
-					'exception' => $e,
-				],
-			);
-			return Utils::withError($response, Constants::HTTP_BAD_REQUEST, 'Failed to create Work object - ' . $e->getMessage());
-		}
-
-		$createResult = $this->worksService->create(
-			workGroupsId: Uuid::fromString($workGroupId),
-			senderUserId: $userId,
-			worksList: $worksList,
-		);
-		if (!$createResult->isError && (!is_array($body) || !array_is_list($body))) {
-			$createResult = RetValueOrError::withValue($createResult->value[0]);
-		}
-		return $createResult->getResponseWithJson($response);
 	}
 
 	public function deleteWork(
@@ -141,18 +92,11 @@ class WorkApi extends AbstractWorkApi
 		ResponseInterface $response,
 		string $workId
 	): ResponseInterface {
-		$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-		if (!Uuid::isValid($workId))
-		{
-			$this->logger->warning("Invalid UUID format ({workId})", ['workId' => $workId]);
-			return Utils::withUuidError($response);
-		}
-
-		return $this->worksService->delete(
-			senderUserId: $userId,
-			worksId: Uuid::fromString($workId),
-		)->getResponseWithJson($response);
+		return $this->apiHandler->delete(
+			request: $request,
+			response: $response,
+			id: $workId,
+		);
 	}
 
 	public function getWork(
@@ -160,18 +104,11 @@ class WorkApi extends AbstractWorkApi
 		ResponseInterface $response,
 		string $workId
 	): ResponseInterface {
-		$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-		if (!Uuid::isValid($workId))
-		{
-			$this->logger->warning("Invalid UUID format ({workId})", ['workId' => $workId]);
-			return Utils::withUuidError($response);
-		}
-
-		return $this->worksService->getOne(
-			senderUserId: $userId,
-			worksId: Uuid::fromString($workId),
-		)->getResponseWithJson($response);
+		return $this->apiHandler->getOne(
+			request: $request,
+			response: $response,
+			id: $workId,
+		);
 	}
 
 	public function getWorkList(
@@ -179,68 +116,22 @@ class WorkApi extends AbstractWorkApi
 		ResponseInterface $response,
 		string $workGroupId
 	): ResponseInterface {
-		$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-		if (!Uuid::isValid($workGroupId))
-		{
-			$this->logger->warning("Invalid UUID format ({workGroupId})", ['workGroupId' => $workGroupId]);
-			return Utils::withUuidError($response);
-		}
-
-		$pagingParams = PagingQueryValidator::withRequest($request, $this->logger);
-		if ($pagingParams->isError) {
-			return $pagingParams->reqError->getResponseWithJson($response);
-		}
-
-		return $this->worksService->getPage(
-			workGroupsId: Uuid::fromString($workGroupId),
-			senderUserId: $userId,
-			pageFrom1: $pagingParams->pageFrom1,
-			perPage: $pagingParams->perPage,
-			topId: $pagingParams->topId,
-		)->getResponseWithJson($response);
+		return $this->apiHandler->getPage(
+			request: $request,
+			response: $response,
+			parentId: $workGroupId,
+		);
 	}
 
 	public function updateWork(
 		ServerRequestInterface $request,
 		ResponseInterface $response,
 		string $workId
-	): ResponseInterface {
-		$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-		if (!Uuid::isValid($workId))
-		{
-			$this->logger->warning("Invalid UUID format ({workId})", ['workId' => $workId]);
-			return Utils::withUuidError($response);
-		}
-
-		$body = $request->getParsedBody();
-		if (is_null($body)) {
-			$this->logger->warning('empty request body');
-			return Utils::withError($response, Constants::HTTP_BAD_REQUEST, 'empty request body');
-		}
-
-		$validateBodyResult = $this->bodyValidator->validate(
-			d: $body,
-			checkRequired: false,
-			allowNestedArray: false,
+		): ResponseInterface {
+		return $this->apiHandler->update(
+			request: $request,
+			response: $response,
+			id: $workId,
 		);
-		if ($validateBodyResult->isError) {
-			$this->logger->warning(
-				'invalid request body: {message}',
-				[
-					'message' => $validateBodyResult->errorMsg,
-				],
-			);
-			return $validateBodyResult->getResponseWithJson($response);
-		}
-		$workData = new Work();
-		$workData->setData($body);
-		return $this->worksService->update(
-			senderUserId: $userId,
-			worksId: Uuid::fromString($workId),
-			data: $workData,
-			requestBody: $body,
-		)->getResponseWithJson($response);
 	}
 }
--- backend/src/trvis_backend/repo/WorksRepo.php
+++ backend/src/trvis_backend/repo/WorksRepo.php
@@ -2,10 +2,7 @@
 
 namespace dev_t0r\trvis_backend\repo;
 
-use dev_t0r\trvis_backend\Constants;
-use dev_t0r\trvis_backend\model\TrvisContentType;
 use dev_t0r\trvis_backend\model\Work;
-use dev_t0r\trvis_backend\RetValueOrError;
 use dev_t0r\trvis_backend\Utils;
 use PDO;
 use PDOStatement;
@@ -13,15 +10,26 @@ use Psr\Log\LoggerInterface;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 
-final class WorksRepo
+/**
+ * @extends MyRepoBase<Work>
+ */
+final class WorksRepo extends MyRepoBase
 {
 	public function __construct(
-		private readonly PDO $db,
-		private readonly LoggerInterface $logger,
+		PDO $db,
+		LoggerInterface $logger,
 	) {
+		parent::__construct(
+			db: $db,
+			logger: $logger,
+			TABLE_NAME: 'works',
+			parentTableNameList: ['work_groups'],
+			SQL_SELECT_COLUMNS: self::SQL_SELECT_COLUMNS,
+			SQL_INSERT_COLUMNS: self::SQL_INSERT_COLUMNS,
+		);
 	}
 
-	private const SQL_COLUMNS = <<<SQL
+	private const SQL_SELECT_COLUMNS = <<<SQL
 		works_id,
 		work_groups_id,
 		created_at,
@@ -37,9 +45,9 @@ final class WorksRepo
 
 	SQL;
 
-	private static function _fetchResultToObj(
-		mixed $d
-	): Work {
+	protected function _fetchResultToObj(
+		mixed $d,
+	): mixed {
 		$result = new Work();
 		$result->setData([
 			'works_id' => Uuid::fromBytes($d['works_id']),
@@ -76,7 +84,7 @@ final class WorksRepo
 		e_train_timetable_file_name
 	)
 	SQL;
-	static function _genInsertValuesQuerySegment(
+	protected function _genInsertValuesQuerySegment(
 		int $i
 	): string {
 		return <<<SQL
@@ -96,549 +104,23 @@ final class WorksRepo
 			)
 		SQL;
 	}
-	static function _setInsertValues(
+	protected function _setInsertValues(
 		PDOStatement $query,
 		int $i,
-		UuidInterface $worksId,
-		Work $work,
+		UuidInterface $id,
+		mixed $d,
 	) {
-		$query->bindValue(":works_id_$i", $worksId->getBytes(), PDO::PARAM_STR);
-		$query->bindValue(":description_$i", $work->description, PDO::PARAM_STR);
-		$query->bindValue(":name_$i", $work->name, PDO::PARAM_STR);
-		$query->bindValue(":affect_date_$i", Utils::utcDateStrOrNull($work->affect_date), PDO::PARAM_STR);
-		$query->bindValue(":affix_content_type_$i", $work->affix_content_type?->value, PDO::PARAM_INT);
+		$query->bindValue(":works_id_$i", $id->getBytes(), PDO::PARAM_STR);
+		$query->bindValue(":description_$i", $d->description, PDO::PARAM_STR);
+		$query->bindValue(":name_$i", $d->name, PDO::PARAM_STR);
+		$query->bindValue(":affect_date_$i", Utils::utcDateStrOrNull($d->affect_date), PDO::PARAM_STR);
+		$query->bindValue(":affix_content_type_$i", $d->affix_content_type?->value, PDO::PARAM_INT);
 		// TODO: implement affix_content
 		$query->bindValue(":affix_file_name_$i", null, PDO::PARAM_STR);
-		$query->bindValue(":remarks_$i", $work->remarks, PDO::PARAM_STR);
-		$query->bindValue(":has_e_train_timetable_$i", $work->has_e_train_timetable, PDO::PARAM_BOOL);
-		$query->bindValue(":e_train_timetable_content_type_$i", $work->e_train_timetable_content_type?->value, PDO::PARAM_INT);
+		$query->bindValue(":remarks_$i", $d->remarks, PDO::PARAM_STR);
+		$query->bindValue(":has_e_train_timetable_$i", $d->has_e_train_timetable, PDO::PARAM_BOOL);
+		$query->bindValue(":e_train_timetable_content_type_$i", $d->e_train_timetable_content_type?->value, PDO::PARAM_INT);
 		// TODO: implement e_train_timetable_content
 		$query->bindValue(":e_train_timetable_file_name_$i", null, PDO::PARAM_STR);
-	}
-
-	/**
-	 * @return RetValueOrError<Work>
-	 */
-	public function selectOne(
-		UuidInterface $worksId,
-	): RetValueOrError {
-		$this->logger->debug(
-			'selectOne workId: {worksId}',
-			[
-				'worksId' => $worksId,
-			],
-		);
-
-		try
-		{
-			$query = $this->db->prepare(
-				'SELECT ' . self::SQL_COLUMNS . <<<SQL
-				FROM
-					works
-				WHERE
-					works_id = :works_id
-				AND
-					deleted_at IS NULL
-				SQL
-			);
-
-			$query->bindValue(':works_id', $worksId->getBytes(), PDO::PARAM_STR);
-
-			$query->execute();
-			$result = $query->fetch(PDO::FETCH_ASSOC);
-			if ($result === false) {
-				$this->logger->warning(
-					'selectOne - rowCount is 0',
-					[
-						'workId' => $worksId,
-					],
-				);
-				return Utils::errWorkNotFound();
-			}
-
-			return RetValueOrError::withValue(self::_fetchResultToObj($result));
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	/**
-	 * @return RetValueOrError<UuidInterface>
-	 */
-	public function selectWorkGroupsId(
-		UuidInterface $worksId,
-	): RetValueOrError {
-		$this->logger->debug(
-			'selectWorkGroupsId workId: {worksId}',
-			[
-				'worksId' => $worksId,
-			],
-		);
-
-		try
-		{
-			$query = $this->db->prepare(
-				<<<SQL
-				SELECT
-					work_groups_id
-				FROM
-					works
-				WHERE
-					works_id = :works_id
-				AND
-					deleted_at IS NULL
-				;
-				SQL
-			);
-
-			$query->bindValue(':works_id', $worksId->getBytes(), PDO::PARAM_STR);
-
-			$query->execute();
-			$result = $query->fetch(PDO::FETCH_ASSOC);
-			if ($result === false) {
-				$this->logger->warning('selectWorkGroupsId - rowCount is 0');
-				return Utils::errWorkNotFound();
-			}
-
-			$resultId = $result['work_groups_id'];
-			return RetValueOrError::withValue(Uuid::fromBytes($resultId));
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	/**
-	 * @return RetValueOrError<array<Work>>
-	 */
-	public function selectPage(
-		UuidInterface $workGroupsId,
-		int $pageFrom1,
-		int $perPage,
-		?UuidInterface $topId,
-	): RetValueOrError {
-		$this->logger->debug(
-			'selectList workGroupsId: {workGroupsId}, pageFrom1: {pageFrom1}, perPage: {perPage}, topId: {topId}',
-			[
-				'workGroupsId' => $workGroupsId,
-				'pageFrom1' => $pageFrom1,
-				'perPage' => $perPage,
-				'topId' => $topId,
-			],
-		);
-
-		$hasTopId = !is_null($topId);
-
-		try
-		{
-			$query = $this->db->prepare(
-				'SELECT ' . self::SQL_COLUMNS . <<<SQL
-				FROM
-					works
-				WHERE
-					work_groups_id = :work_groups_id
-				AND
-					deleted_at IS NULL
-				SQL
-				.
-				(!$hasTopId ? ' ' : ' AND works_id <= :top_id ')
-				.
-				<<<SQL
-				ORDER BY
-					works_id DESC
-				LIMIT
-					:perPage
-				OFFSET
-					:offset
-				SQL
-			);
-
-			$query->bindValue(':work_groups_id', $workGroupsId->getBytes(), PDO::PARAM_STR);
-			if ($hasTopId) {
-				$query->bindValue(':top_id', $topId);
-			}
-			$query->bindValue(':perPage', $perPage, PDO::PARAM_INT);
-			$query->bindValue(':offset', ($pageFrom1 - 1) * $perPage, PDO::PARAM_INT);
-
-			$query->execute();
-			$this->logger->debug(
-				'rorCount: {rowCount}',
-				[
-					'rowCount' => $query->rowCount(),
-				],
-			);
-			$result = $query->fetchAll(PDO::FETCH_ASSOC);
-			if ($result === false) {
-				$this->logger->warning('selectList - rowCount is 0');
-				return Utils::errWorkNotFound();
-			}
-
-			$works = array_map(
-				fn($data) => self::_fetchResultToObj($data),
-				$result,
-			);
-
-			return RetValueOrError::withValue($works);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	/**
-	 * @return RetValueOrError<array<Work>>
-	 */
-	public function selectList(
-		/** @param array<UuidInterface> $worksIdList */
-		array $worksIdList,
-	): RetValueOrError {
-		$this->logger->debug(
-			"selectList worksIdList: {worksIdList}",
-			[
-				"worksIdList" => $worksIdList,
-			],
-		);
-
-		$worksIdListCount = count($worksIdList);
-		$placeholders = implode(',', array_map(
-			fn($i) => ":works_id_$i",
-			range(0, $worksIdListCount - 1),
-		));
-
-		try
-		{
-			$query = $this->db->prepare(
-				'SELECT ' . self::SQL_COLUMNS . <<<SQL
-				FROM
-					works
-				WHERE
-					works_id IN ($placeholders)
-				SQL
-			);
-
-			for ($i = 0; $i < $worksIdListCount; $i++) {
-				$query->bindValue(":works_id_$i", $worksIdList[$i]->getBytes(), PDO::PARAM_STR);
-			}
-
-			$query->execute();
-			$this->logger->debug(
-				'rorCount: {rowCount}',
-				[
-					'rowCount' => $query->rowCount(),
-				],
-			);
-			$result = $query->fetchAll(PDO::FETCH_ASSOC);
-			if ($result === false) {
-				$this->logger->warning('selectList - rowCount is 0');
-				return Utils::errWorkNotFound();
-			}
-
-			$works = array_map(
-				fn($data) => self::_fetchResultToObj($data),
-				$result,
-			);
-
-			return RetValueOrError::withValue($works);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	/**
-	 * @return RetValueOrError<null>
-	 */
-	public function insertList(
-		UuidInterface $workGroupsId,
-		string $ownerUserId,
-		/** @param array<UuidInterface> $works */
-		array $worksIdList,
-		/** @param array<Work> $works */
-		array $works,
-	): RetValueOrError {
-		$this->logger->debug(
-			'insertList workGroupsId: {workGroupsId}, works: {works}',
-			[
-				'workGroupsId' => $workGroupsId,
-				'works' => $works,
-			],
-		);
-
-		try
-		{
-			$worksCount = count($works);
-			$query = $this->db->prepare(
-				'INSERT INTO works'
-				.
-				self::SQL_INSERT_COLUMNS
-				.
-				' VALUES '
-				.
-				implode(',', array_map(
-					fn($i) => self::_genInsertValuesQuerySegment($i),
-					range(0, $worksCount - 1),
-				))
-			);
-			$query->bindValue(':work_groups_id', $workGroupsId->getBytes(), PDO::PARAM_STR);
-			$query->bindValue(':owner', $ownerUserId, PDO::PARAM_STR);
-			for ($i = 0; $i < $worksCount; $i++) {
-				self::_setInsertValues($query, $i, $worksIdList[$i], $works[$i]);
-			}
-
-			$query->execute();
-			$rowCount = $query->rowCount();
-			$this->logger->debug(
-				'insertList - rowCount: {rowCount}',
-				[
-					'rowCount' => $rowCount,
-				],
-			);
-			return RetValueOrError::withValue(null);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	/**
-	 * @return RetValueOrError<null>
-	 */
-	public function update(
-		UuidInterface $worksId,
-		array $worksProps,
-	): RetValueOrError {
-		$this->logger->debug(
-			'updateList worksId: {worksId}, works: {works}',
-			[
-				'worksId' => $worksId,
-				'works' => $worksProps,
-			],
-		);
-
-		if (count($worksProps) === 0) {
-			return RetValueOrError::withValue(null);
-		}
-
-		try
-		{
-			$query = $this->db->prepare(
-				"UPDATE works SET "
-				.
-				implode(',', array_map(fn($key) => "{$key} = :{$key}", array_keys($worksProps)))
-				.
-				" WHERE works_id = :works_id AND deleted_at IS NULL"
-			);
-			$query->bindValue(':works_id', $worksId->getBytes(), PDO::PARAM_STR);
-			foreach ($worksProps as $key => $value) {
-				$paramType = PDO::PARAM_STR;
-				if ($key === 'has_e_train_timetable') {
-					$paramType = PDO::PARAM_BOOL;
-				} elseif ($key === 'affix_content_type' || $key === 'e_train_timetable_content_type') {
-					$paramType = PDO::PARAM_INT;
-				}
-				$query->bindValue(":{$key}", $value, $paramType);
-			}
-
-			$query->execute();
-			return RetValueOrError::withValue(null);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	public function deleteOne(
-		UuidInterface $worksId,
-	): RetValueOrError {
-		$this->logger->debug(
-			"deleteOne worksId: {worksId}",
-			[
-				"worksId" => $worksId,
-			],
-		);
-
-		try
-		{
-			$query = $this->db->prepare(<<<SQL
-				UPDATE
-					works
-				SET
-					deleted_at = CURRENT_TIMESTAMP()
-				WHERE
-					works_id = :works_id
-				AND
-					deleted_at IS NULL
-				;
-				SQL
-			);
-
-			$query->bindValue(":works_id", $worksId->getBytes(), PDO::PARAM_STR);
-			$query->execute();
-			$rowCount = $query->rowCount();
-			$this->logger->debug(
-				"deleteByWorkGroupsId - rowCount: {rowCount}",
-				[
-					"rowCount" => $rowCount,
-				],
-			);
-			if ($rowCount === 0) {
-				$this->logger->warning(
-					"Work not found ({worksId})",
-					[
-						"worksId" => $worksId,
-					],
-				);
-				return Utils::errWorkNotFound();
-			}
-			return RetValueOrError::withValue(null);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	public function deleteByWorkGroupsId(
-		UuidInterface $workGroupsId,
-	): RetValueOrError {
-		$this->logger->debug(
-			"deleteByWorkGroupsId workGroupsId: {workGroupsId}",
-			[
-				"workGroupsId" => $workGroupsId,
-			],
-		);
-
-		try
-		{
-			$query = $this->db->prepare(<<<SQL
-				UPDATE
-					works
-				SET
-					deleted_at = CURRENT_TIMESTAMP()
-				WHERE
-					work_groups_id = :work_groups_id
-				AND
-					deleted_at IS NULL
-				;
-				SQL
-			);
-
-			$query->bindValue(":work_groups_id", $workGroupsId->getBytes(), PDO::PARAM_STR);
-			$query->execute();
-			$this->logger->debug(
-				"deleteByWorkGroupsId - rowCount: {rowCount}",
-				[
-					"rowCount" => $query->rowCount(),
-				],
-			);
-			return RetValueOrError::withValue(null);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
 	}
 }
