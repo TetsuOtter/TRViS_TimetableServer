9570c5536025f9af667bb1715b39a823fa8039ca
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2024-01-10 22:46:40 +0900

    StationTrack周りの実装を共通化

--- backend/src/trvis_backend/api/StationTrackApi.php
+++ backend/src/trvis_backend/api/StationTrackApi.php
@@ -2,46 +2,43 @@
 
 namespace dev_t0r\trvis_backend\api;
 
-use dev_t0r\trvis_backend\auth\MyAuthMiddleware;
-use dev_t0r\trvis_backend\Constants;
 use dev_t0r\trvis_backend\model\StationTrack;
-use dev_t0r\trvis_backend\RetValueOrError;
 use dev_t0r\trvis_backend\service\StationTracksService;
-use dev_t0r\trvis_backend\Utils;
 use dev_t0r\trvis_backend\validator\IntValidationRule;
-use dev_t0r\trvis_backend\validator\PagingQueryValidator;
 use dev_t0r\trvis_backend\validator\RequestValidator;
 use PDO;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Log\LoggerInterface;
-use Ramsey\Uuid\Uuid;
 
 final class StationTrackApi extends AbstractStationTrackApi
 {
-	private readonly StationTracksService $stationTracksService;
-	private readonly RequestValidator $bodyValidator;
+	private readonly MyApiHandler $apiHandler;
 	public function __construct(
-		private PDO $db,
+		PDO $db,
 		private readonly LoggerInterface $logger,
 	) {
-		$this->stationTracksService = new StationTracksService($db, $logger);
-		$this->bodyValidator = new RequestValidator(
-			RequestValidator::getNameValidationRule(),
-			RequestValidator::getDescriptionValidationRule(),
-			new IntValidationRule(
-				key: 'run_in_limit',
-				isNullable: true,
-				isRequired: false,
-				minValue: 1,
-				maxValue: 999,
-			),
-			new IntValidationRule(
-				key: 'run_out_limit',
-				isNullable: true,
-				isRequired: false,
-				minValue: 1,
-				maxValue: 999,
+		$this->apiHandler = new MyApiHandler(
+			service: new StationTracksService($db, $logger),
+			logger: $logger,
+			modelClassName: StationTrack::class,
+			bodyValidator: new RequestValidator(
+				RequestValidator::getNameValidationRule(),
+				RequestValidator::getDescriptionValidationRule(),
+				new IntValidationRule(
+					key: 'run_in_limit',
+					isNullable: true,
+					isRequired: false,
+					minValue: 1,
+					maxValue: 999,
+				),
+				new IntValidationRule(
+					key: 'run_out_limit',
+					isNullable: true,
+					isRequired: false,
+					minValue: 1,
+					maxValue: 999,
+				),
 			),
 		);
 	}
@@ -51,164 +48,58 @@ final class StationTrackApi extends AbstractStationTrackApi
 		ResponseInterface $response,
 		string $stationId,
 	): ResponseInterface {
-		$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-		if (!Uuid::isValid($stationId))
-		{
-			$this->logger->warning("Invalid UUID format ({stationId})", ['stationId' => $stationId]);
-			return Utils::withUuidError($response);
-		}
-
-		$body = $request->getParsedBody();
-		$validateResult = $this->bodyValidator->validate(
-			d: $body,
-			checkRequired: true,
-			allowNestedArray: true,
+		return $this->apiHandler->create(
+			request: $request,
+			response: $response,
+			parentId: $stationId,
 		);
-		if ($validateResult->isError) {
-			$this->logger->error(
-				"invalid input: {message}",
-				[
-					"message" => $validateResult->errorMsg,
-				],
-			);
-			return $validateResult->getResponseWithJson($response);
-		}
-
-		try
-		{
-			$stationTracksList = array_map(function ($req) {
-				$stationTrack = new StationTrack();
-				$stationTrack->setData($req);
-				return $stationTrack;
-			}, (is_array($body) && array_is_list($body)) ? $body : [$body]);
-		}
-		catch (\Throwable $e)
-		{
-			$this->logger->error(
-				'Failed to create StationTrack object: {exception}',
-				[
-					'exception' => $e,
-				],
-			);
-			return Utils::withError($response, Constants::HTTP_BAD_REQUEST, 'Failed to create StationTrack object - ' . $e->getMessage());
-		}
-
-		$createResult = $this->stationTracksService->create(
-			stationsId: Uuid::fromString($stationId),
-			senderUserId: $userId,
-			stationTracksList: $stationTracksList,
-		);
-		if (!$createResult->isError && (!is_array($body) || !array_is_list($body))) {
-			$createResult = RetValueOrError::withValue($createResult->value[0]);
-		}
-		return $createResult->getResponseWithJson($response);	}
+	}
 
 	public function deleteStationTrack(
 		ServerRequestInterface $request,
 		ResponseInterface $response,
 		string $stationTrackId,
-		): ResponseInterface {
-			$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-			if (!Uuid::isValid($stationTrackId))
-			{
-				$this->logger->warning("Invalid UUID format ({stationTrackId})", ['stationTrackId' => $stationTrackId]);
-				return Utils::withUuidError($response);
-			}
-
-			return $this->stationTracksService->delete(
-				senderUserId: $userId,
-				stationTracksId: Uuid::fromString($stationTrackId),
-			)->getResponseWithJson($response);
-		}
+	): ResponseInterface {
+		return $this->apiHandler->delete(
+			request: $request,
+			response: $response,
+			id: $stationTrackId,
+		);
+	}
 
 	public function getStationTrack(
 		ServerRequestInterface $request,
 		ResponseInterface $response,
 		string $stationTrackId,
-		): ResponseInterface {
-			$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-			if (!Uuid::isValid($stationTrackId))
-			{
-				$this->logger->warning("Invalid UUID format ({stationTrackId})", ['stationTrackId' => $stationTrackId]);
-				return Utils::withUuidError($response);
-			}
-
-			return $this->stationTracksService->getOne(
-				senderUserId: $userId,
-				stationTracksId: Uuid::fromString($stationTrackId),
-			)->getResponseWithJson($response);
-		}
+	): ResponseInterface {
+		return $this->apiHandler->getOne(
+			request: $request,
+			response: $response,
+			id: $stationTrackId,
+		);
+	}
 
 	public function getStationTrackList(
 		ServerRequestInterface $request,
 		ResponseInterface $response,
 		string $stationId,
-		): ResponseInterface {
-			$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-			if (!Uuid::isValid($stationId))
-			{
-				$this->logger->warning("Invalid UUID format ({stationId})", ['stationId' => $stationId]);
-				return Utils::withUuidError($response);
-			}
-
-			$pagingParams = PagingQueryValidator::withRequest($request, $this->logger);
-			if ($pagingParams->isError) {
-				return $pagingParams->reqError->getResponseWithJson($response);
-			}
-
-			return $this->stationTracksService->getPage(
-				stationsId: Uuid::fromString($stationId),
-				senderUserId: $userId,
-				pageFrom1: $pagingParams->pageFrom1,
-				perPage: $pagingParams->perPage,
-				topId: $pagingParams->topId,
-			)->getResponseWithJson($response);
-		}
+	): ResponseInterface {
+		return $this->apiHandler->getPage(
+			request: $request,
+			response: $response,
+			parentId: $stationId,
+		);
+	}
 
 	public function updateStationTrack(
 		ServerRequestInterface $request,
 		ResponseInterface $response,
 		string $stationTrackId,
-		): ResponseInterface {
-			$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-			if (!Uuid::isValid($stationTrackId))
-			{
-				$this->logger->warning("Invalid UUID format ({stationTrackId})", ['stationTrackId' => $stationTrackId]);
-				return Utils::withUuidError($response);
-			}
-
-			$body = $request->getParsedBody();
-			if (is_null($body)) {
-				$this->logger->warning('empty request body');
-				return Utils::withError($response, Constants::HTTP_BAD_REQUEST, 'empty request body');
-			}
-
-			$validateBodyResult = $this->bodyValidator->validate(
-				d: $body,
-				checkRequired: false,
-				allowNestedArray: false,
-			);
-			if ($validateBodyResult->isError) {
-				$this->logger->warning(
-					'invalid request body: {message}',
-					[
-						'message' => $validateBodyResult->errorMsg,
-					],
-				);
-				return $validateBodyResult->getResponseWithJson($response);
-			}
-			$stationTrackData = new StationTrack();
-			$stationTrackData->setData($body);
-			return $this->stationTracksService->update(
-				senderUserId: $userId,
-				stationTracksId: Uuid::fromString($stationTrackId),
-				data: $stationTrackData,
-				requestBody: $body,
-			)->getResponseWithJson($response);
-		}
+	): ResponseInterface {
+		return $this->apiHandler->update(
+			request: $request,
+			response: $response,
+			id: $stationTrackId,
+		);
 	}
+}
--- backend/src/trvis_backend/repo/StationTracksRepo.php
+++ backend/src/trvis_backend/repo/StationTracksRepo.php
@@ -2,9 +2,7 @@
 
 namespace dev_t0r\trvis_backend\repo;
 
-use dev_t0r\trvis_backend\Constants;
 use dev_t0r\trvis_backend\model\StationTrack;
-use dev_t0r\trvis_backend\RetValueOrError;
 use dev_t0r\trvis_backend\Utils;
 use PDO;
 use PDOStatement;
@@ -12,12 +10,23 @@ use Psr\Log\LoggerInterface;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 
-final class StationTracksRepo
+/**
+ * @extends MyRepoBase<StationTrack>
+ */
+final class StationTracksRepo extends MyRepoBase
 {
 	public function __construct(
-		private readonly PDO $db,
-		private readonly LoggerInterface $logger,
+		PDO $db,
+		LoggerInterface $logger,
 	) {
+		parent::__construct(
+			db: $db,
+			logger: $logger,
+			TABLE_NAME: 'station_tracks',
+			parentTableNameList: ['stations', 'work_groups'],
+			SQL_SELECT_COLUMNS: self::SQL_SELECT_COLUMNS,
+			SQL_INSERT_COLUMNS: self::SQL_INSERT_COLUMNS,
+		);
 	}
 
 	private const SQL_SELECT_COLUMNS = <<<SQL
@@ -31,9 +40,9 @@ final class StationTracksRepo
 
 	SQL;
 
-	private static function _fetchResultToObj(
+	protected function _fetchResultToObj(
 		mixed $d,
-	): StationTrack {
+	): mixed {
 		$result = new StationTrack();
 		$result->setData([
 			'station_tracks_id' => Uuid::fromBytes($d['station_tracks_id']),
@@ -58,619 +67,32 @@ final class StationTracksRepo
 		run_out_limit
 	)
 	SQL;
-	static function _genInsertValuesQuerySegment(
+	protected function _genInsertValuesQuerySegment(
 		int $i
 	): string {
 		return <<<SQL
 			(
 				:station_tracks_id_{$i},
-				:stations_id,
+				{$this->PLACEHOLDER_PARENT_ID},
 				:description_{$i},
-				:owner,
+				{$this->PLACEHOLDER_OWNER},
+				:name_{$i},
 				:name_{$i},
 				:run_in_limit_{$i},
 				:run_out_limit_{$i}
 			)
 		SQL;
 	}
-	static function _setInsertValues(
+	protected function _setInsertValues(
 		PDOStatement $query,
 		int $i,
-		UuidInterface $stationTracksId,
-		StationTrack $stationTrack,
+		UuidInterface $id,
+		mixed $d,
 	) {
-		$query->bindValue(":station_tracks_id_$i", $stationTracksId->getBytes(), PDO::PARAM_STR);
-		$query->bindValue(":description_$i", $stationTrack->description, PDO::PARAM_STR);
-		$query->bindValue(":name_$i", $stationTrack->name, PDO::PARAM_STR);
-		$query->bindValue(":run_in_limit_$i", $stationTrack->run_in_limit, PDO::PARAM_INT);
-		$query->bindValue(":run_out_limit_$i", $stationTrack->run_out_limit, PDO::PARAM_INT);
-	}
-
-	/**
-	 * @return RetValueOrError<StationTrack>
-	 */
-	public function selectOne(
-		UuidInterface $stationTracksId,
-	): RetValueOrError {
-		$this->logger->debug(
-			'selectOne stationTracksId: {stationTracksId}',
-			[
-				'stationTracksId' => $stationTracksId,
-			],
-		);
-
-		try
-		{
-			$query = $this->db->prepare(
-				'SELECT ' . self::SQL_SELECT_COLUMNS . <<<SQL
-					FROM station_tracks
-				WHERE
-					station_tracks.station_tracks_id = :station_tracks_id
-				AND
-					station_tracks.deleted_at IS NULL
-				SQL
-			);
-
-			$query->bindValue(':station_tracks_id', $stationTracksId->getBytes(), PDO::PARAM_STR);
-
-			$query->execute();
-			$result = $query->fetch(PDO::FETCH_ASSOC);
-			if ($result === false) {
-				$this->logger->warning(
-					'selectOne({stationTracksId}) - rowCount is 0',
-					[
-						'stationTracksId' => $stationTracksId,
-					],
-				);
-				return Utils::errStationTrackNotFound();
-			}
-
-			return RetValueOrError::withValue(self::_fetchResultToObj($result));
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	/**
-	 * @return RetValueOrError<UuidInterface>
-	 */
-	public function selectWorkGroupsId(
-		UuidInterface $stationTracksId,
-	): RetValueOrError {
-		$this->logger->debug(
-			'selectWorkGroupsId stationTracksId: {stationTracksId}',
-			[
-				'stationTracksId' => $stationTracksId,
-			],
-		);
-
-		try
-		{
-			$query = $this->db->prepare(
-				<<<SQL
-				SELECT
-					stations.work_groups_id
-				FROM
-					stations
-				JOIN
-					station_tracks
-				USING
-					(stations_id)
-				WHERE
-					station_tracks.station_tracks_id = :station_tracks_id
-				AND
-					stations.deleted_at IS NULL
-				AND
-					station_tracks.deleted_at IS NULL
-				;
-				SQL
-			);
-
-			$query->bindValue(':station_tracks_id', $stationTracksId->getBytes(), PDO::PARAM_STR);
-
-			$query->execute();
-			$result = $query->fetch(PDO::FETCH_ASSOC);
-			if ($result === false) {
-				$this->logger->warning('selectWorkGroupsId - rowCount is 0');
-				return Utils::errStationTrackNotFound();
-			}
-
-			$resultId = $result['work_groups_id'];
-			return RetValueOrError::withValue(Uuid::fromBytes($resultId));
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	/**
-	 * @return RetValueOrError<array<StationTrack>>
-	 */
-	public function selectPage(
-		UuidInterface $stationsId,
-		int $pageFrom1,
-		int $perPage,
-		?UuidInterface $topId,
-	): RetValueOrError {
-		$this->logger->debug(
-			'selectList stationsId: {stationsId}, pageFrom1: {pageFrom1}, perPage: {perPage}, topId: {topId}',
-			[
-				'stationsId' => $stationsId,
-				'pageFrom1' => $pageFrom1,
-				'perPage' => $perPage,
-				'topId' => $topId,
-			],
-		);
-
-		$hasTopId = !is_null($topId);
-
-		try
-		{
-			$query = $this->db->prepare(
-				'SELECT ' . self::SQL_SELECT_COLUMNS
-				.
-				<<<SQL
-				FROM
-					station_tracks
-				WHERE
-					station_tracks.stations_id = :stations_id
-				AND
-					station_tracks.deleted_at IS NULL
-				SQL
-				.
-				(!$hasTopId ? ' ' : ' AND station_tracks.station_tracks_id <= :top_id ')
-				.
-				<<<SQL
-				ORDER BY
-					station_tracks_id DESC
-				LIMIT
-					:perPage
-				OFFSET
-					:offset
-				SQL
-			);
-
-			$query->bindValue(':stations_id', $stationsId->getBytes(), PDO::PARAM_STR);
-			if ($hasTopId) {
-				$query->bindValue(':top_id', $topId);
-			}
-			$query->bindValue(':perPage', $perPage, PDO::PARAM_INT);
-			$query->bindValue(':offset', ($pageFrom1 - 1) * $perPage, PDO::PARAM_INT);
-
-			$query->execute();
-			$this->logger->debug(
-				'rorCount: {rowCount}',
-				[
-					'rowCount' => $query->rowCount(),
-				],
-			);
-			$result = $query->fetchAll(PDO::FETCH_ASSOC);
-			if ($result === false) {
-				$this->logger->warning('selectList - rowCount is 0');
-				return Utils::errStationTrackNotFound();
-			}
-
-			$stationTracks = array_map(
-				fn($data) => self::_fetchResultToObj($data),
-				$result,
-			);
-
-			return RetValueOrError::withValue($stationTracks);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	/**
-	 * @return RetValueOrError<array<StationTrack>>
-	 */
-	public function selectList(
-		/** @param array<UuidInterface> $stationTracksIdList */
-		array $stationTracksIdList,
-	): RetValueOrError {
-		$this->logger->debug(
-			"selectList stationTracksIdList: {stationTracksIdList}",
-			[
-				"stationTracksIdList" => $stationTracksIdList,
-			],
-		);
-
-		$stationTracksIdListCount = count($stationTracksIdList);
-		$placeholders = implode(',', array_map(
-			fn($i) => ":station_tracks_id_$i",
-			range(0, $stationTracksIdListCount - 1),
-		));
-
-		try
-		{
-			$query = $this->db->prepare(
-				'SELECT ' . self::SQL_SELECT_COLUMNS . <<<SQL
-				FROM
-					station_tracks
-				WHERE
-					station_tracks_id IN ($placeholders)
-				SQL
-			);
-
-			for ($i = 0; $i < $stationTracksIdListCount; $i++) {
-				$query->bindValue(":station_tracks_id_$i", $stationTracksIdList[$i]->getBytes(), PDO::PARAM_STR);
-			}
-
-			$query->execute();
-			$this->logger->debug(
-				'rorCount: {rowCount}',
-				[
-					'rowCount' => $query->rowCount(),
-				],
-			);
-			$result = $query->fetchAll(PDO::FETCH_ASSOC);
-			if ($result === false) {
-				$this->logger->warning('selectList - rowCount is 0');
-				return Utils::errStationTrackNotFound();
-			}
-
-			$stationTracks = array_map(
-				fn($data) => self::_fetchResultToObj($data),
-				$result,
-			);
-
-			return RetValueOrError::withValue($stationTracks);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	/**
-	 * @return RetValueOrError<null>
-	 */
-	public function insertList(
-		UuidInterface $stationsId,
-		string $ownerUserId,
-		/** @param array<UuidInterface> $stationTracksIdList */
-		array $stationTracksIdList,
-		/** @param array<StationTrack> $stationTracks */
-		array $stationTracks,
-	): RetValueOrError {
-		$this->logger->debug(
-			'insertList stationsId: {stationsId}, stationTracks: {stationTracks}',
-			[
-				'stationsId' => $stationsId,
-				'stationTracks' => $stationTracks,
-			],
-		);
-
-		try
-		{
-			$stationTracksCount = count($stationTracks);
-			$query = $this->db->prepare(
-				'INSERT INTO station_tracks'
-				.
-				self::SQL_INSERT_COLUMNS
-				.
-				' VALUES '
-				.
-				implode(',', array_map(
-					fn($i) => self::_genInsertValuesQuerySegment($i),
-					range(0, $stationTracksCount - 1),
-				))
-			);
-			$query->bindValue(':stations_id', $stationsId->getBytes(), PDO::PARAM_STR);
-			$query->bindValue(':owner', $ownerUserId, PDO::PARAM_STR);
-			for ($i = 0; $i < $stationTracksCount; $i++) {
-				self::_setInsertValues($query, $i, $stationTracksIdList[$i], $stationTracks[$i]);
-			}
-
-			$query->execute();
-			$rowCount = $query->rowCount();
-			$this->logger->debug(
-				'insertList - rowCount: {rowCount}',
-				[
-					'rowCount' => $rowCount,
-				],
-			);
-			return RetValueOrError::withValue(null);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	/**
-	 * @return RetValueOrError<null>
-	 */
-	public function update(
-		UuidInterface $stationTracksId,
-		array $stationTracksProps,
-	): RetValueOrError {
-		$this->logger->debug(
-			'updateList stationTracksId: {stationTracksId}, stationTracks: {stationTracks}',
-			[
-				'stationTracksId' => $stationTracksId,
-				'stationTracks' => $stationTracksProps,
-			],
-		);
-
-		if (count($stationTracksProps) === 0) {
-			return RetValueOrError::withValue(null);
-		}
-
-		try
-		{
-			$query = $this->db->prepare(
-				"UPDATE station_tracks SET "
-				.
-				implode(',', array_map(fn($key) => "{$key} = :{$key}", array_keys($stationTracksProps)))
-				.
-				" WHERE station_tracks_id = :station_tracks_id AND deleted_at IS NULL"
-			);
-			$query->bindValue(':station_tracks_id', $stationTracksId->getBytes(), PDO::PARAM_STR);
-			foreach ($stationTracksProps as $key => $value) {
-				$paramType = PDO::PARAM_STR;
-				if ($key === 'run_in_limit' || $key === 'run_out_limit') {
-					$paramType = PDO::PARAM_INT;
-				}
-				$query->bindValue(":{$key}", $value, $paramType);
-			}
-
-			$query->execute();
-			return RetValueOrError::withValue(null);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	public function deleteOne(
-		UuidInterface $stationTracksId,
-	): RetValueOrError {
-		$this->logger->debug(
-			"deleteOne stationTracksId: {stationTracksId}",
-			[
-				"stationTracksId" => $stationTracksId,
-			],
-		);
-
-		try
-		{
-			$query = $this->db->prepare(<<<SQL
-				UPDATE
-					station_tracks
-				SET
-					deleted_at = CURRENT_TIMESTAMP()
-				WHERE
-					station_tracks_id = :station_tracks_id
-				AND
-					deleted_at IS NULL
-				;
-				SQL
-			);
-
-			$query->bindValue(":station_tracks_id", $stationTracksId->getBytes(), PDO::PARAM_STR);
-			$query->execute();
-			$rowCount = $query->rowCount();
-			$this->logger->debug(
-				"deleteOne - rowCount: {rowCount}",
-				[
-					"rowCount" => $rowCount,
-				],
-			);
-			if ($rowCount === 0) {
-				$this->logger->warning(
-					"StationTrack not found ({stationTracksId})",
-					[
-						"stationTracksId" => $stationTracksId,
-					],
-				);
-				return Utils::errStationTrackNotFound();
-			}
-			return RetValueOrError::withValue(null);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	public function deleteByStationsId(
-		UuidInterface $stationsId,
-	): RetValueOrError {
-		$this->logger->debug(
-			"deleteByStationsId stationsId: {stationsId}",
-			[
-				"stationsId" => $stationsId,
-			],
-		);
-
-		try
-		{
-			$query = $this->db->prepare(<<<SQL
-				UPDATE
-					station_tracks
-				SET
-					deleted_at = CURRENT_TIMESTAMP()
-				WHERE
-					stations_id = :stations_id
-				AND
-					deleted_at IS NULL
-				;
-				SQL
-			);
-
-			$query->bindValue(":stations_id", $stationsId->getBytes(), PDO::PARAM_STR);
-			$query->execute();
-			$this->logger->debug(
-				"deleteByStationsId - rowCount: {rowCount}",
-				[
-					"rowCount" => $query->rowCount(),
-				],
-			);
-			return RetValueOrError::withValue(null);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	public function deleteByWorkGroupsId(
-		UuidInterface $workGroupsId,
-	): RetValueOrError {
-		$this->logger->debug(
-			"deleteByWorkGroupsId workGroupsId: {workGroupsId}",
-			[
-				"workGroupsId" => $workGroupsId,
-			],
-		);
-
-		try
-		{
-			$query = $this->db->prepare(<<<SQL
-				UPDATE
-					station_tracks
-				JOIN
-					stations
-				USING
-					(stations_id)
-				SET
-					station_tracks.deleted_at = CURRENT_TIMESTAMP()
-				WHERE
-					stations.work_groups_id = :work_groups_id
-				AND
-					station_tracks.deleted_at IS NULL
-				;
-				SQL
-			);
-
-			$query->bindValue(":work_groups_id", $workGroupsId->getBytes(), PDO::PARAM_STR);
-			$query->execute();
-			$this->logger->debug(
-				"deleteByWorkGroupsId - rowCount: {rowCount}",
-				[
-					"rowCount" => $query->rowCount(),
-				],
-			);
-			return RetValueOrError::withValue(null);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
+		$query->bindValue(":station_tracks_id_$i", $id->getBytes(), PDO::PARAM_STR);
+		$query->bindValue(":description_$i", $d->description, PDO::PARAM_STR);
+		$query->bindValue(":name_$i", $d->name, PDO::PARAM_STR);
+		$query->bindValue(":run_in_limit_$i", $d->run_in_limit, PDO::PARAM_INT);
+		$query->bindValue(":run_out_limit_$i", $d->run_out_limit, PDO::PARAM_INT);
 	}
 }
