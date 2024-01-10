4ab25511d83509dcb5e996bd18851e3250d1ed86
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2024-01-10 23:09:59 +0900

    Train周りの実装も共通化

--- backend/src/trvis_backend/api/TrainApi.php
+++ backend/src/trvis_backend/api/TrainApi.php
@@ -2,110 +2,108 @@
 
 namespace dev_t0r\trvis_backend\api;
 
-use dev_t0r\trvis_backend\auth\MyAuthMiddleware;
 use dev_t0r\trvis_backend\Constants;
 use dev_t0r\trvis_backend\model\Train;
-use dev_t0r\trvis_backend\RetValueOrError;
 use dev_t0r\trvis_backend\service\TrainsService;
-use dev_t0r\trvis_backend\Utils;
 use dev_t0r\trvis_backend\validator\BoolValidationRule;
 use dev_t0r\trvis_backend\validator\IntValidationRule;
-use dev_t0r\trvis_backend\validator\PagingQueryValidator;
 use dev_t0r\trvis_backend\validator\RequestValidator;
 use dev_t0r\trvis_backend\validator\StringValidationRule;
 use PDO;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Log\LoggerInterface;
-use Ramsey\Uuid\Uuid;
 
 final class TrainApi extends AbstractTrainApi
 {
-	private readonly TrainsService $trainsService;
-	private readonly RequestValidator $requestValidator;
+	private readonly MyApiHandler $apiHandler;
 	public function __construct(
-		private PDO $db,
+		PDO $db,
 		private readonly LoggerInterface $logger,
 	) {
-		$this->trainsService = new TrainsService($db, $logger);
-		$this->requestValidator = new RequestValidator(
-			RequestValidator::getDescriptionValidationRule(),
-			new StringValidationRule(
-				key: 'train_number',
-				minLength: Constants::NAME_MIN_LENGTH,
-				maxLength: Constants::NAME_MAX_LENGTH,
-				isRequired: true,
-				isNullable: false,
-			),
-			new StringValidationRule(
-				key: 'max_speed',
-				maxLength: 255,
-				isNullable: true,
-			),
-			new StringValidationRule(
-				key: 'speed_type',
-				maxLength: 255,
-				isNullable: true,
-			),
-			new StringValidationRule(
-				key: 'nominal_tractive_capacity',
-				maxLength: 255,
-				isNullable: true,
-			),
-			new IntValidationRule(
-				key: 'car_count',
-				isNullable: true,
-			),
-			new StringValidationRule(
-				key: 'destination',
-				maxLength: 255,
-				isNullable: true,
-			),
-			new StringValidationRule(
-				key: 'begin_remarks',
-				maxLength: 255,
-				isNullable: true,
-			),
-			new StringValidationRule(
-				key: 'after_remarks',
-				maxLength: 255,
-				isNullable: true,
-			),
-			new StringValidationRule(
-				key: 'remarks',
-				maxLength: 255,
-				isNullable: true,
-			),
-			new StringValidationRule(
-				key: 'before_departure',
-				maxLength: 255,
-				isNullable: true,
-			),
-			new StringValidationRule(
-				key: 'after_arrive',
-				maxLength: 255,
-				isNullable: true,
-			),
-			new StringValidationRule(
-				key: 'train_info',
-				maxLength: 255,
-				isNullable: true,
-			),
-			new IntValidationRule(
-				key: 'direction',
-				isRequired: true,
-				isNullable: false,
-			),
-			new IntValidationRule(
-				key: 'day_count',
-				minValue: 0,
-				isRequired: true,
-				isNullable: false,
-			),
-			new BoolValidationRule(
-				key: 'is_ride_on_moving',
-				isNullable: true,
-			),
+		$this->apiHandler = new MyApiHandler(
+			service: new TrainsService($db, $logger),
+			logger: $logger,
+			modelClassName: Train::class,
+			bodyValidator: new RequestValidator(
+				RequestValidator::getDescriptionValidationRule(),
+				new StringValidationRule(
+					key: 'train_number',
+					minLength: Constants::NAME_MIN_LENGTH,
+					maxLength: Constants::NAME_MAX_LENGTH,
+					isRequired: true,
+					isNullable: false,
+				),
+				new StringValidationRule(
+					key: 'max_speed',
+					maxLength: 255,
+					isNullable: true,
+				),
+				new StringValidationRule(
+					key: 'speed_type',
+					maxLength: 255,
+					isNullable: true,
+				),
+				new StringValidationRule(
+					key: 'nominal_tractive_capacity',
+					maxLength: 255,
+					isNullable: true,
+				),
+				new IntValidationRule(
+					key: 'car_count',
+					isNullable: true,
+				),
+				new StringValidationRule(
+					key: 'destination',
+					maxLength: 255,
+					isNullable: true,
+				),
+				new StringValidationRule(
+					key: 'begin_remarks',
+					maxLength: 255,
+					isNullable: true,
+				),
+				new StringValidationRule(
+					key: 'after_remarks',
+					maxLength: 255,
+					isNullable: true,
+				),
+				new StringValidationRule(
+					key: 'remarks',
+					maxLength: 255,
+					isNullable: true,
+				),
+				new StringValidationRule(
+					key: 'before_departure',
+					maxLength: 255,
+					isNullable: true,
+				),
+				new StringValidationRule(
+					key: 'after_arrive',
+					maxLength: 255,
+					isNullable: true,
+				),
+				new StringValidationRule(
+					key: 'train_info',
+					maxLength: 255,
+					isNullable: true,
+				),
+				new IntValidationRule(
+					key: 'direction',
+					isRequired: true,
+					isNullable: false,
+				),
+				new IntValidationRule(
+					key: 'day_count',
+					minValue: 0,
+					isRequired: true,
+					isNullable: false,
+				),
+				new BoolValidationRule(
+					key: 'is_ride_on_moving',
+					isNullable: true,
+				),
+				),
 		);
 	}
 
@@ -114,159 +112,58 @@ final class TrainApi extends AbstractTrainApi
 		ResponseInterface $response,
 		string $workId,
 	): ResponseInterface {
-		$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-		if (!Uuid::isValid($workId))
-		{
-			$this->logger->warning("Invalid UUID format ({workId})", ['workId' => $workId]);
-			return Utils::withUuidError($response);
-		}
-
-		$body = $request->getParsedBody();
-		$validateResult = $this->requestValidator->validate(
-			d: $body,
-			checkRequired: true,
-			allowNestedArray: true,
+		return $this->apiHandler->create(
+			request: $request,
+			response: $response,
+			parentId: $workId,
 		);
-		if ($validateResult->isError) {
-			$this->logger->error(
-				"invalid input: {message}",
-				[
-					"message" => $validateResult->errorMsg,
-				],
-			);
-			return $validateResult->getResponseWithJson($response);
-		}
-
-		try
-		{
-			$trainsList = array_map(function ($req) {
-				$train = new Train();
-				$train->setData($req);
-				return $train;
-			}, (is_array($body) && array_is_list($body)) ? $body : [$body]);
-		}
-		catch (\Throwable $e)
-		{
-			$this->logger->error(
-				'Failed to create Train object: {exception}',
-				[
-					'exception' => $e,
-				],
-			);
-			return Utils::withError($response, Constants::HTTP_BAD_REQUEST, 'Failed to create Train object - ' . $e->getMessage());
-		}
-
-		$createResult = $this->trainsService->create(
-			worksId: Uuid::fromString($workId),
-			senderUserId: $userId,
-			trainsList: $trainsList,
-		);
-		if (!$createResult->isError && (!is_array($body) || !array_is_list($body))) {
-			$createResult = RetValueOrError::withValue($createResult->value[0]);
-		}
-		return $createResult->getResponseWithJson($response);	}
+	}
 
 	public function deleteTrain(
 		ServerRequestInterface $request,
 		ResponseInterface $response,
 		string $trainId,
-		): ResponseInterface {
-			$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-			if (!Uuid::isValid($trainId))
-			{
-				$this->logger->warning("Invalid UUID format ({trainId})", ['trainId' => $trainId]);
-				return Utils::withUuidError($response);
-			}
-
-			return $this->trainsService->delete(
-				senderUserId: $userId,
-				trainsId: Uuid::fromString($trainId),
-			)->getResponseWithJson($response);
-		}
+	): ResponseInterface {
+		return $this->apiHandler->delete(
+			request: $request,
+			response: $response,
+			id: $trainId,
+		);
+	}
 
 	public function getTrain(
 		ServerRequestInterface $request,
 		ResponseInterface $response,
 		string $trainId,
-		): ResponseInterface {
-			$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-			if (!Uuid::isValid($trainId))
-			{
-				$this->logger->warning("Invalid UUID format ({trainId})", ['trainId' => $trainId]);
-				return Utils::withUuidError($response);
-			}
-
-			return $this->trainsService->getOne(
-				senderUserId: $userId,
-				trainsId: Uuid::fromString($trainId),
-			)->getResponseWithJson($response);
-		}
+	): ResponseInterface {
+		return $this->apiHandler->getOne(
+			request: $request,
+			response: $response,
+			id: $trainId,
+		);
+	}
 
 	public function getTrainList(
 		ServerRequestInterface $request,
 		ResponseInterface $response,
 		string $workId,
-		): ResponseInterface {
-			$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-			if (!Uuid::isValid($workId))
-			{
-				$this->logger->warning("Invalid UUID format ({workId})", ['workId' => $workId]);
-				return Utils::withUuidError($response);
-			}
-
-			$pagingParams = PagingQueryValidator::withRequest($request, $this->logger);
-			if ($pagingParams->isError) {
-				return $pagingParams->reqError->getResponseWithJson($response);
-			}
-
-			return $this->trainsService->getPage(
-				worksId: Uuid::fromString($workId),
-				senderUserId: $userId,
-				pageFrom1: $pagingParams->pageFrom1,
-				perPage: $pagingParams->perPage,
-				topId: $pagingParams->topId,
-			)->getResponseWithJson($response);
-		}
+	): ResponseInterface {
+		return $this->apiHandler->getPage(
+			request: $request,
+			response: $response,
+			parentId: $workId,
+		);
+	}
 
 	public function updateTrain(
 		ServerRequestInterface $request,
 		ResponseInterface $response,
 		string $trainId,
-		): ResponseInterface {
-			$userId = MyAuthMiddleware::getUserIdOrAnonymous($request);
-
-			if (!Uuid::isValid($trainId))
-			{
-				$this->logger->warning("Invalid UUID format ({trainId})", ['trainId' => $trainId]);
-				return Utils::withUuidError($response);
-			}
-
-			$body = $request->getParsedBody();
-			$validateBodyResult = $this->requestValidator->validate(
-				d: $body,
-				checkRequired: false,
-				allowNestedArray: false,
-			);
-				if ($validateBodyResult->isError) {
-				$this->logger->warning(
-					'invalid request body: {message}',
-					[
-						'message' => $validateBodyResult->errorMsg,
-					],
-				);
-				return $validateBodyResult->getResponseWithJson($response);
-			}
-			$trainData = new Train();
-			$trainData->setData($body);
-			return $this->trainsService->update(
-				senderUserId: $userId,
-				trainsId: Uuid::fromString($trainId),
-				data: $trainData,
-				requestBody: $body,
-			)->getResponseWithJson($response);
-		}
+	): ResponseInterface {
+		return $this->apiHandler->update(
+			request: $request,
+			response: $response,
+			id: $trainId,
+		);
 	}
+}
--- backend/src/trvis_backend/repo/TrainsRepo.php
+++ backend/src/trvis_backend/repo/TrainsRepo.php
@@ -2,9 +2,7 @@
 
 namespace dev_t0r\trvis_backend\repo;
 
-use dev_t0r\trvis_backend\Constants;
 use dev_t0r\trvis_backend\model\Train;
-use dev_t0r\trvis_backend\RetValueOrError;
 use dev_t0r\trvis_backend\Utils;
 use PDO;
 use PDOStatement;
@@ -12,12 +10,23 @@ use Psr\Log\LoggerInterface;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 
-final class TrainsRepo
+/**
+ * @extends MyRepoBase<Train>
+ */
+final class TrainsRepo extends MyRepoBase
 {
 	public function __construct(
-		private readonly PDO $db,
-		private readonly LoggerInterface $logger,
+		PDO $db,
+		LoggerInterface $logger,
 	) {
+		parent::__construct(
+			db: $db,
+			logger: $logger,
+			TABLE_NAME: 'trains',
+			parentTableNameList: ['works', 'work_groups'],
+			SQL_SELECT_COLUMNS: self::SQL_SELECT_COLUMNS,
+			SQL_INSERT_COLUMNS: self::SQL_INSERT_COLUMNS,
+		);
 	}
 
 	private const SQL_SELECT_COLUMNS = <<<SQL
@@ -43,9 +52,9 @@ final class TrainsRepo
 
 	SQL;
 
-	private static function _fetchResultToObj(
+	protected function _fetchResultToObj(
 		mixed $d,
-	): Train {
+	): mixed {
 		$result = new Train();
 		$result->setData([
 			'trains_id' => Uuid::fromBytes($d['trains_id']),
@@ -94,15 +103,15 @@ final class TrainsRepo
 		is_ride_on_moving
 	)
 	SQL;
-	static function _genInsertValuesQuerySegment(
+	protected function _genInsertValuesQuerySegment(
 		int $i
 	): string {
 		return <<<SQL
 			(
 				:trains_id_{$i},
-				:works_id,
+				{$this->PLACEHOLDER_PARENT_ID},
 				:description_{$i},
-				:owner,
+				{$this->PLACEHOLDER_OWNER},
 				:train_number_{$i},
 				:max_speed_{$i},
 				:speed_type_{$i},
@@ -121,13 +130,13 @@ final class TrainsRepo
 			)
 		SQL;
 	}
-	static function _setInsertValues(
+	protected function _setInsertValues(
 		PDOStatement $query,
 		int $i,
-		UuidInterface $trainsId,
-		Train $d,
+		UuidInterface $id,
+		mixed $d,
 	) {
-		$query->bindValue(":trains_id_$i", $trainsId->getBytes(), PDO::PARAM_STR);
+		$query->bindValue(":trains_id_$i", $id->getBytes(), PDO::PARAM_STR);
 		$query->bindValue(":description_$i", $d->description, PDO::PARAM_STR);
 		$query->bindValue(":train_number_$i", $d->train_number, PDO::PARAM_STR);
 		$query->bindValue(":max_speed_$i", $d->max_speed, PDO::PARAM_STR);
@@ -144,592 +153,5 @@ final class TrainsRepo
 		$query->bindValue(":direction_$i", $d->direction, PDO::PARAM_INT);
 		$query->bindValue(":day_count_$i", $d->day_count, PDO::PARAM_INT);
 		$query->bindValue(":is_ride_on_moving_$i", $d->is_ride_on_moving, PDO::PARAM_BOOL);
-	}
-
-	/**
-	 * @return RetValueOrError<Train>
-	 */
-	public function selectOne(
-		UuidInterface $trainsId,
-	): RetValueOrError {
-		$this->logger->debug(
-			'selectOne trainsId: {trainsId}',
-			[
-				'trainsId' => $trainsId,
-			],
-		);
-
-		try
-		{
-			$query = $this->db->prepare(
-				'SELECT ' . self::SQL_SELECT_COLUMNS . <<<SQL
-				FROM
-					trains
-				WHERE
-					trains.trains_id = :trains_id
-				AND
-					trains.deleted_at IS NULL
-				SQL
-			);
-
-			$query->bindValue(':trains_id', $trainsId->getBytes(), PDO::PARAM_STR);
-
-			$query->execute();
-			$result = $query->fetch(PDO::FETCH_ASSOC);
-			if ($result === false) {
-				$this->logger->warning(
-					'selectOne({trainsId}) - rowCount is 0',
-					[
-						'trainsId' => $trainsId,
-					],
-				);
-				return Utils::errTrainNotFound();
-			}
-
-			return RetValueOrError::withValue(self::_fetchResultToObj($result));
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	/**
-	 * @return RetValueOrError<UuidInterface>
-	 */
-	public function selectWorkGroupsId(
-		UuidInterface $trainsId,
-	): RetValueOrError {
-		$this->logger->debug(
-			'selectWorkGroupsId trainsId: {trainsId}',
-			[
-				'trainsId' => $trainsId,
-			],
-		);
-
-		try
-		{
-			$query = $this->db->prepare(
-				<<<SQL
-				SELECT
-					works.work_groups_id
-				FROM
-					works
-				JOIN
-					trains
-				USING
-					(works_id)
-				WHERE
-					trains.trains_id = :trains_id
-				AND
-					works.deleted_at IS NULL
-				AND
-					trains.deleted_at IS NULL
-				;
-				SQL
-			);
-
-			$query->bindValue(':trains_id', $trainsId->getBytes(), PDO::PARAM_STR);
-
-			$query->execute();
-			$result = $query->fetch(PDO::FETCH_ASSOC);
-			if ($result === false) {
-				$this->logger->warning('selectWorkGroupsId - rowCount is 0');
-				return Utils::errTrainNotFound();
-			}
-
-			$resultId = $result['work_groups_id'];
-			return RetValueOrError::withValue(Uuid::fromBytes($resultId));
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	/**
-	 * @return RetValueOrError<array<Train>>
-	 */
-	public function selectPage(
-		UuidInterface $worksId,
-		int $pageFrom1,
-		int $perPage,
-		?UuidInterface $topId,
-	): RetValueOrError {
-		$this->logger->debug(
-			'selectList worksId: {worksId}, pageFrom1: {pageFrom1}, perPage: {perPage}, topId: {topId}',
-			[
-				'worksId' => $worksId,
-				'pageFrom1' => $pageFrom1,
-				'perPage' => $perPage,
-				'topId' => $topId,
-			],
-		);
-
-		$hasTopId = !is_null($topId);
-
-		try
-		{
-			$query = $this->db->prepare(
-				'SELECT ' . self::SQL_SELECT_COLUMNS . <<<SQL
-				FROM
-					trains
-				WHERE
-					trains.works_id = :works_id
-				AND
-					trains.deleted_at IS NULL
-				SQL
-				.
-				(!$hasTopId ? ' ' : ' AND trains.trains_id <= :top_id ')
-				.
-				<<<SQL
-				ORDER BY
-					trains_id DESC
-				LIMIT
-					:perPage
-				OFFSET
-					:offset
-				SQL
-			);
-
-			$query->bindValue(':works_id', $worksId->getBytes(), PDO::PARAM_STR);
-			if ($hasTopId) {
-				$query->bindValue(':top_id', $topId);
-			}
-			$query->bindValue(':perPage', $perPage, PDO::PARAM_INT);
-			$query->bindValue(':offset', ($pageFrom1 - 1) * $perPage, PDO::PARAM_INT);
-
-			$query->execute();
-			$this->logger->debug(
-				'rorCount: {rowCount}',
-				[
-					'rowCount' => $query->rowCount(),
-				],
-			);
-			$result = $query->fetchAll(PDO::FETCH_ASSOC);
-			if ($result === false) {
-				$this->logger->warning('selectList - rowCount is 0');
-				return Utils::errTrainNotFound();
-			}
-
-			$trains = array_map(
-				fn($data) => self::_fetchResultToObj($data),
-				$result,
-			);
-
-			return RetValueOrError::withValue($trains);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	/**
-	 * @return RetValueOrError<array<Train>>
-	 */
-	public function selectList(
-		/** @param array<UuidInterface> $trainsIdList */
-		array $trainsIdList,
-	): RetValueOrError {
-		$this->logger->debug(
-			"selectList trainsIdList: {trainsIdList}",
-			[
-				"trainsIdList" => $trainsIdList,
-			],
-		);
-
-		$trainsIdListCount = count($trainsIdList);
-		$placeholders = implode(',', array_map(
-			fn($i) => ":trains_id_$i",
-			range(0, $trainsIdListCount - 1),
-		));
-
-		try
-		{
-			$query = $this->db->prepare(
-				'SELECT ' . self::SQL_SELECT_COLUMNS . <<<SQL
-				FROM
-					trains
-				WHERE
-					trains_id IN ($placeholders)
-				SQL
-			);
-
-			for ($i = 0; $i < $trainsIdListCount; $i++) {
-				$query->bindValue(":trains_id_$i", $trainsIdList[$i]->getBytes(), PDO::PARAM_STR);
-			}
-
-			$query->execute();
-			$this->logger->debug(
-				'rorCount: {rowCount}',
-				[
-					'rowCount' => $query->rowCount(),
-				],
-			);
-			$result = $query->fetchAll(PDO::FETCH_ASSOC);
-			if ($result === false) {
-				$this->logger->warning('selectList - rowCount is 0');
-				return Utils::errTrainNotFound();
-			}
-
-			$trains = array_map(
-				fn($data) => self::_fetchResultToObj($data),
-				$result,
-			);
-
-			return RetValueOrError::withValue($trains);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	/**
-	 * @return RetValueOrError<null>
-	 */
-	public function insertList(
-		UuidInterface $worksId,
-		string $ownerUserId,
-		/** @param array<UuidInterface> $trainsIdList */
-		array $trainsIdList,
-		/** @param array<Train> $trains */
-		array $trains,
-	): RetValueOrError {
-		$this->logger->debug(
-			'insertList worksId: {worksId}, trains: {trains}',
-			[
-				'worksId' => $worksId,
-				'trains' => $trains,
-			],
-		);
-
-		try
-		{
-			$trainsCount = count($trains);
-			$query = $this->db->prepare(
-				'INSERT INTO trains'
-				.
-				self::SQL_INSERT_COLUMNS
-				.
-				' VALUES '
-				.
-				implode(',', array_map(
-					fn($i) => self::_genInsertValuesQuerySegment($i),
-					range(0, $trainsCount - 1),
-				))
-			);
-			$query->bindValue(':works_id', $worksId->getBytes(), PDO::PARAM_STR);
-			$query->bindValue(':owner', $ownerUserId, PDO::PARAM_STR);
-			for ($i = 0; $i < $trainsCount; $i++) {
-				self::_setInsertValues($query, $i, $trainsIdList[$i], $trains[$i]);
-			}
-
-			$query->execute();
-			$rowCount = $query->rowCount();
-			$this->logger->debug(
-				'insertList - rowCount: {rowCount}',
-				[
-					'rowCount' => $rowCount,
-				],
-			);
-			return RetValueOrError::withValue(null);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	/**
-	 * @return RetValueOrError<null>
-	 */
-	public function update(
-		UuidInterface $trainsId,
-		array $trainsProps,
-	): RetValueOrError {
-		$this->logger->debug(
-			'updateList trainsId: {trainsId}, trains: {trains}',
-			[
-				'trainsId' => $trainsId,
-				'trains' => $trainsProps,
-			],
-		);
-
-		if (count($trainsProps) === 0) {
-			return RetValueOrError::withValue(null);
-		}
-
-		try
-		{
-			$query = $this->db->prepare(
-				"UPDATE trains SET "
-				.
-				implode(',', array_map(fn($key) => "{$key} = :{$key}", array_keys($trainsProps)))
-				.
-				" WHERE trains_id = :trains_id AND deleted_at IS NULL"
-			);
-			$query->bindValue(':trains_id', $trainsId->getBytes(), PDO::PARAM_STR);
-			foreach ($trainsProps as $key => $value) {
-				$paramType = PDO::PARAM_STR;
-				if ($key === 'run_in_limit' || $key === 'run_out_limit') {
-					$paramType = PDO::PARAM_INT;
-				}
-				$query->bindValue(":{$key}", $value, $paramType);
-			}
-
-			$query->execute();
-			return RetValueOrError::withValue(null);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	public function deleteOne(
-		UuidInterface $trainsId,
-	): RetValueOrError {
-		$this->logger->debug(
-			"deleteOne trainsId: {trainsId}",
-			[
-				"trainsId" => $trainsId,
-			],
-		);
-
-		try
-		{
-			$query = $this->db->prepare(<<<SQL
-				UPDATE
-					trains
-				SET
-					deleted_at = CURRENT_TIMESTAMP()
-				WHERE
-					trains_id = :trains_id
-				AND
-					deleted_at IS NULL
-				;
-				SQL
-			);
-
-			$query->bindValue(":trains_id", $trainsId->getBytes(), PDO::PARAM_STR);
-			$query->execute();
-			$rowCount = $query->rowCount();
-			$this->logger->debug(
-				"deleteOne - rowCount: {rowCount}",
-				[
-					"rowCount" => $rowCount,
-				],
-			);
-			if ($rowCount === 0) {
-				$this->logger->warning(
-					"Train not found ({trainsId})",
-					[
-						"trainsId" => $trainsId,
-					],
-				);
-				return Utils::errTrainNotFound();
-			}
-			return RetValueOrError::withValue(null);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	public function deleteByWorksId(
-		UuidInterface $worksId,
-	): RetValueOrError {
-		$this->logger->debug(
-			"deleteByWorksId worksId: {worksId}",
-			[
-				"worksId" => $worksId,
-			],
-		);
-
-		try
-		{
-			$query = $this->db->prepare(<<<SQL
-				UPDATE
-					trains
-				SET
-					deleted_at = CURRENT_TIMESTAMP()
-				WHERE
-					works_id = :works_id
-				AND
-					deleted_at IS NULL
-				;
-				SQL
-			);
-
-			$query->bindValue(":works_id", $worksId->getBytes(), PDO::PARAM_STR);
-			$query->execute();
-			$this->logger->debug(
-				"deleteByWorksId - rowCount: {rowCount}",
-				[
-					"rowCount" => $query->rowCount(),
-				],
-			);
-			return RetValueOrError::withValue(null);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
-	}
-
-	public function deleteByWorkGroupsId(
-		UuidInterface $workGroupsId,
-	): RetValueOrError {
-		$this->logger->debug(
-			"deleteByWorkGroupsId workGroupsId: {workGroupsId}",
-			[
-				"workGroupsId" => $workGroupsId,
-			],
-		);
-
-		try
-		{
-			$query = $this->db->prepare(<<<SQL
-				UPDATE
-					trains
-				JOIN
-					works
-				USING
-					(works_id)
-				SET
-					trains.deleted_at = CURRENT_TIMESTAMP()
-				WHERE
-					works.work_groups_id = :work_groups_id
-				AND
-					trains.deleted_at IS NULL
-				;
-				SQL
-			);
-
-			$query->bindValue(":work_groups_id", $workGroupsId->getBytes(), PDO::PARAM_STR);
-			$query->execute();
-			$this->logger->debug(
-				"deleteByWorkGroupsId - rowCount: {rowCount}",
-				[
-					"rowCount" => $query->rowCount(),
-				],
-			);
-			return RetValueOrError::withValue(null);
-		}
-		catch (\PDOException $ex)
-		{
-			$errCode = $ex->getCode();
-
-			$this->logger->error(
-				"Failed to execute SQL ({errorCode} -> {errorInfo})",
-				[
-					"errorCode" => $errCode,
-					"errorInfo" => $ex->getMessage(),
-				],
-			);
-			return RetValueOrError::withError(
-				Constants::HTTP_INTERNAL_SERVER_ERROR,
-				"Failed to execute SQL - " . $errCode,
-			);
-		}
 	}
 }
