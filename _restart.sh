bfb1c85f69eb845c682d1e03aa9cb1b2efd9fe11
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2024-01-14 21:43:11 +0900

    サービスのリビルド・再起動スクリプトを作成

---
+++ _restart.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied" 1>&2
+    exit 1
+fi
+
+SERVICE_NAME=$1
+
+docker compose rm -fs $SERVICE_NAME
+
+docker compose build $SERVICE_NAME
+
+docker compose up -d $SERVICE_NAME
