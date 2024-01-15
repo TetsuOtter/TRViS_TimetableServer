2efa84ad480791104ad62911b934319ecc11ee3d
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2024-01-16 01:14:28 +0900

    Light / Dark Theme対応

---
+++ frontend/src/hooks/appThemeModeHook.ts
@@ -0,0 +1,10 @@
+import { useMediaQuery } from "@mui/material";
+import { useAppSelector } from "../redux/hooks";
+import { paletteModeSelector } from "../redux/selectors/systemSelector";
+
+export const useAppThemeMode = () => {
+	const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+	const paletteMode = useAppSelector(paletteModeSelector);
+
+	return paletteMode ?? (prefersDarkMode ? "dark" : "light");
+};
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -8,6 +8,11 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import ErrorPage from "./pages/ErrorPage.tsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme, CssBaseline } from "@mui/material";
+import { useAppThemeMode } from "./hooks/appThemeModeHook.ts";
 
 const router = createBrowserRouter([
 	{
@@ -17,8 +22,31 @@ const router = createBrowserRouter([
 	},
 ]);
 
+const RootComponentWithRedux = () => {
+	const appThemeMode = useAppThemeMode();
+
+	const theme = useMemo(
+		() =>
+			createTheme({
+				palette: {
+					mode: appThemeMode,
+				},
+			}),
+		[appThemeMode]
+	);
+
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<RouterProvider router={router} />
+		</ThemeProvider>
+	);
+};
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<Provider store={store}>
+			<RootComponentWithRedux />
+		</Provider>
 	</React.StrictMode>
 );
