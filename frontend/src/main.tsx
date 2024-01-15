8aea61ebfdbc05aa04ec99fa59247efa7139eb1f
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2024-01-15 23:14:27 +0900

    react-router-domをインストール

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import ErrorPage from "./pages/ErrorPage.tsx";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <ErrorPage />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
