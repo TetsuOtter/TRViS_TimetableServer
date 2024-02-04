49c5b85c86ff23e896350bef2ab5bd4ed42af36a
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2024-02-04 22:27:54 +0900

    パス取得関数を修正

--- frontend/src/utils/getPathString.ts
+++ frontend/src/utils/getPathString.ts
@@ -1,5 +1,7 @@
 export const WORK_GROUPS_ID_PLACEHOLDER_KEY = "workGroupsId";
 
 export const getPathToWorkGroupList = () => "/work_groups";
+export const getPathToWorkGroupOne = (workGroupsId: string) =>
+	`/work_groups/${workGroupsId}`;
 export const getPathToWorkList = (workGroupsId: string) =>
-	`${getPathToWorkGroupList()}/${workGroupsId}/works`;
+	`${getPathToWorkGroupOne(workGroupsId)}/works`;
