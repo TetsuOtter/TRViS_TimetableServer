7f228daa3a09a296208a8743d18349d842243410
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2024-02-14 23:36:16 +0900

    WorkGroupListページへのリンクを追加
    毎度リンク手打ちで移動するのが面倒なため

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,25 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 import { useSelector } from "react-redux";
 
 import { apiInfoApiSelector } from "./redux/selectors/apiSelector";
+import { getPathToWorkGroupList } from "./utils/getPathString";
 
 const MSG = "Hello World!";
+const GO_TO_WORK_GROUPS_TEXT = "WorkGroups";
 
 const App = () => {
 	const api = useSelector(apiInfoApiSelector);
 	useEffect(() => {
 		api.getApiInfo().then(console.log).catch(console.error);
 	}, [api]);
-	return <h1>{MSG}</h1>;
+	return (
+		<>
+			<h1>{MSG}</h1>
+			<Link to={getPathToWorkGroupList()}>{GO_TO_WORK_GROUPS_TEXT}</Link>
+		</>
+	);
 };
 
 export default App;
